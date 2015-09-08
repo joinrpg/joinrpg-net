df64d80570de8430cfa03e33fe1fc4024667b7e4
Leonid Tsarev <leo@bastilia.ru>
2015-09-08 15:30:02 +0300

    Fix namespaces in ProjectCreateViewModel

--- Joinrpg/Models/ProjectCreateViewModel.cs
+++ Joinrpg/Models/ProjectCreateViewModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace JoinRpg.Web.Models
+﻿namespace JoinRpg.Web.Models
 {
   public class ProjectCreateViewModel
   {
