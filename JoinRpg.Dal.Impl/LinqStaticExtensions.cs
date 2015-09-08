6b71379a12d9bd696b1c6e2684267c630bc4be29
Leonid Tsarev <leo@bastilia.ru>
2015-09-08 14:14:14 +0300

    Incude some useful LINQ collection functions

---
+++ JoinRpg.Dal.Impl/LinqStaticExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JoinRpg.Dal.Impl
+{
+  public static class LinqStaticExtensions
+  {
+    /// <summary>
+    /// This is required by LINQ to correctly change links list. Simple <code>entity.TargetList = newValue</code> will not work (LINQ will add NEW links).
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="target"></param>
+    /// <param name="newValue"></param>
+    public static void AssignLinksList<T>(this ICollection<T> target, ICollection<T> newValue)
+    {
+      foreach (var parent in target.Except(newValue).ToList())
+      {
+        target.Remove(parent);
+      }
+      target.AddLinkList(newValue);
+    }
+
+    public static void AddLinkList<T>(this ICollection<T> target, IEnumerable<T> newValue)
+    {
+      foreach (var parent in newValue.Except(target).ToList())
+      {
+        target.Add(parent);
+      }
+    }
+
+    public static void CleanLinksList<T>(this ICollection<T> target)
+    {
+      target.AssignLinksList(new List<T>());
+    }
+  }
+}
