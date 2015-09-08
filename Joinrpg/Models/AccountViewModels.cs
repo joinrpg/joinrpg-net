d1c7380e183d1b1bfa071a29b764198eafa58fa2
Leonid Tsarev <leo@bastilia.ru>
2015-09-08 22:59:26 +0300

    Fix messages for login

--- Joinrpg/Models/AccountViewModels.cs
+++ Joinrpg/Models/AccountViewModels.cs
@@ -55,10 +55,10 @@ namespace JoinRpg.Web.Models
 
         [Required]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Запомнить меня?")]
         public bool RememberMe { get; set; }
     }
 
@@ -72,11 +72,11 @@ namespace JoinRpg.Web.Models
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
-        [Display(Name = "Password")]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm password")]
+        [Display(Name = "Еще раз?")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
