74bbbce95401354873ccbbfd558bc066bad80955
Leonid Tsarev <leo@bastilia.ru>
2015-09-08 14:13:12 +0300

    Add some labels to AddGameObjectViewModelBase

--- Joinrpg/Models/AddGameObjectViewModelBase.cs
+++ Joinrpg/Models/AddGameObjectViewModelBase.cs
@@ -1,24 +1,24 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using JoinRpg.DataModel;
 
 namespace JoinRpg.Web.Models
 {
   public class AddGameObjectViewModelBase
   {
-    [Required]
     public int ProjectId { get; set; }
 
-    [Required]
+    [Required, DisplayName("Является частью локаций")]
     public List<int> ParentCharacterGroupIds { get; set; } = new List<int>();
 
     [Required]
     public string Name
     { get; set; }
 
+    [DisplayName("Публично?")]
     public bool IsPublic { get; set; } = true;
 
-    [DataType(DataType.MultilineText)]
+    [DataType(DataType.MultilineText),DisplayName("Описание")]
     public string Description { get; set; }
     public CharacterGroupListViewModel Data { get; set; }
   }
