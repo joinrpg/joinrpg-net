fcbd352c73b5595c3eff8a0de3aba81665db2875
Leonid Tsarev <leo@bastilia.ru>
2015-09-09 14:06:39 +0300

    Introduce fullname

--- JoinRpg.DataModel/User.cs
+++ JoinRpg.DataModel/User.cs
@@ -1,28 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using JoinRpg.Helpers;
 using Microsoft.AspNet.Identity;
 
 namespace JoinRpg.DataModel
 {
   public class User : IUser<int>
   {
-    public int UserId
-    { get; set; }
+    public int UserId { get; set; }
 
-    public string BornName
-    { get; set; }
+    public string BornName { get; set; }
 
-    public string FatherName
-    { get; set; }
+    public string FatherName { get; set; }
 
-    public string SurName
-    { get; set; }
+    public string SurName { get; set; }
 
-    public int? LegacyAllRpgInp
-    { get; set; }
+    public int? LegacyAllRpgInp { get; set; }
 
     public int Id => UserId;
     public string UserName { get; set; }
@@ -34,8 +26,10 @@ namespace JoinRpg.DataModel
 
     public virtual ICollection<ProjectAcl> ProjectAcls { get; set; }
 
-    public virtual ICollection<Claim>  Claims { get; set; }
+    public virtual ICollection<Claim> Claims { get; set; }
 
     public string DisplayName => UserName; //Should create creative display name
+
+    public string FullName => new[] {BornName, FatherName, SurName}.JoinIfNotNullOrWhitespace(" ");
   }
 }
---
+++ JoinRpg.Helpers/StaticStringHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace JoinRpg.Helpers
+{
+  public static class StaticStringHelpers
+  {
+    public static string JoinIfNotNullOrWhitespace(this IEnumerable<string> strings, string separator)
+    {
+      return string.Join(separator, strings.Select(string.IsNullOrWhiteSpace));
+    }
+  }
+}
