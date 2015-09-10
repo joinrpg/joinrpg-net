7a59c81aef5e9bfec58ec6509c9e6f1d00430b01
Leonid Tsarev <leo@bastilia.ru>
2015-09-10 22:33:36 +0300

    Add Enum.GetAttribute helper

---
+++ JoinRpg.Helpers/StaticEnumHelpers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace JoinRpg.Helpers
+{
+  public static class StaticEnumHelpers
+  {
+    public static TAttribute GetAttribute<TAttribute>(this Enum enumValue) where TAttribute:Attribute
+    {
+      return enumValue.GetType()
+        .GetMember(enumValue.ToString())
+        .First()
+        .GetCustomAttribute<TAttribute>();
+    }
+  }
+
+}
