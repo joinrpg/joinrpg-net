aef33180a15d635760313df6f1e7c8b18bca07dc
Leonid Tsarev <leo@bastilia.ru>
2015-09-10 21:12:20 +0300

    Add more string & collection helper functions

---
+++ JoinRpg.Helpers/StaticCollectionHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+
+namespace JoinRpg.Helpers
+{
+  public static class StaticCollectionHelpers
+  {
+    public static Dictionary<string, string> ToDictionary(this NameValueCollection collection)
+    {
+      return collection.AllKeys.ToDictionary(key => key, key => collection[key]);
+    }
+  }
+}
--- JoinRpg.Helpers/StaticStringHelpers.cs
+++ JoinRpg.Helpers/StaticStringHelpers.cs
@@ -1,13 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using JetBrains.Annotations;
 
 namespace JoinRpg.Helpers
 {
   public static class StaticStringHelpers
   {
-    public static string JoinIfNotNullOrWhitespace(this IEnumerable<string> strings, string separator)
+    public static string JoinIfNotNullOrWhitespace([NotNull] this IEnumerable<string> strings, [NotNull] string separator)
     {
       return string.Join(separator, strings.Where(string.IsNullOrWhiteSpace));
+    }
+
+    /// <summary>
+    /// Return only strings that have specified prefix (and with this prefix removed)
+    /// </summary>
+    public static IEnumerable<string> SelectWherePrefix([NotNull] this IEnumerable<string> enumerable, [NotNull] string prefix)
+    {
+      return enumerable.Where(key => key.StartsWith(prefix)).Select(key=> key.Substring(prefix.Length));
+    }
+
+    public static IEnumerable<int> UnprefixNumbers(this IEnumerable<string> enumerable, string prefix)
+    {
+      return enumerable.SelectWherePrefix(prefix).Select(int.Parse);
     }
   }
 }
