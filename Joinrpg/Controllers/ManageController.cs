41f918c0614b36cbfa861f6e69cab8ef8c2ad543
Leonid Tsarev <leo@bastilia.ru>
2015-09-18 20:49:46 +0300

    Reformat manage controller

--- Joinrpg/Controllers/ManageController.cs
+++ Joinrpg/Controllers/ManageController.cs
@@ -9,57 +9,115 @@ using Microsoft.Owin.Security;
 
 namespace JoinRpg.Web.Controllers
 {
-    [Authorize]
-    public class ManageController : Common.ControllerBase
+  [Authorize]
+  public class ManageController : Common.ControllerBase
+  {
+    private readonly ApplicationSignInManager _signInManager;
+
+    public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+      : base(userManager)
     {
-        private ApplicationSignInManager _signInManager;
-        
-        public ManageController(ApplicationUserManager userManager, ApplicationSignInManager signInManager) : base(userManager)
-        {
-            SignInManager = signInManager;
-        }
+      _signInManager = signInManager;
+    }
 
-        public ApplicationSignInManager SignInManager
-        {
-            get
-            {
-                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
-            }
-            private set 
-            { 
-                _signInManager = value; 
-            }
-        }
+    private ApplicationSignInManager SignInManager
+      => _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
 
 
     [Authorize]
-        //
-        // GET: /Manage/Index
-        public async Task<ActionResult> Index(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Ваш пароль был изменен."
-                : message == ManageMessageId.SetPasswordSuccess ? "Ваш пароль был установлен."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : "";
+    //
+    // GET: /Manage/Index
+    public async Task<ActionResult> Index(ManageMessageId? message)
+    {
+      ViewBag.StatusMessage =
+        message == ManageMessageId.ChangePasswordSuccess
+          ? "Ваш пароль был изменен."
+          : message == ManageMessageId.SetPasswordSuccess
+            ? "Ваш пароль был установлен."
+            : message == ManageMessageId.Error
+              ? "An error has occurred."
+              : "";
 
-            var userId = CurrentUserId;
-            var model = new IndexViewModel
-            {
-                HasPassword = HasPassword(),
-                Logins = await UserManager.GetLoginsAsync(userId),
-            };
-            return View(model);
-        }
-
-        //
-        // POST: /Manage/RemoveLogin
-      [HttpPost]
-      [ValidateAntiForgeryToken]
-      public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
+      var userId = CurrentUserId;
+      var model = new IndexViewModel
       {
-        ManageMessageId? message;
-        var result = await UserManager.RemoveLoginAsync(CurrentUserId, new UserLoginInfo(loginProvider, providerKey));
+        HasPassword = HasPassword(),
+        Logins = await UserManager.GetLoginsAsync(userId),
+      };
+      return View(model);
+    }
+
+    //
+    // POST: /Manage/RemoveLogin
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> RemoveLogin(string loginProvider, string providerKey)
+    {
+      ManageMessageId? message;
+      var result = await UserManager.RemoveLoginAsync(CurrentUserId, new UserLoginInfo(loginProvider, providerKey));
+      if (result.Succeeded)
+      {
+        var user = await UserManager.FindByIdAsync(CurrentUserId);
+        if (user != null)
+        {
+          await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+        }
+        message = ManageMessageId.RemoveLoginSuccess;
+      }
+      else
+      {
+        message = ManageMessageId.Error;
+      }
+      return RedirectToAction("ManageLogins", new {Message = message});
+    }
+
+    //
+    // GET: /Manage/ChangePassword
+    public ActionResult ChangePassword()
+    {
+      return View();
+    }
+
+    //
+    // POST: /Manage/ChangePassword
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      var result = await UserManager.ChangePasswordAsync(CurrentUserId, model.OldPassword, model.NewPassword);
+      if (result.Succeeded)
+      {
+        var user = await UserManager.FindByIdAsync(CurrentUserId);
+        if (user != null)
+        {
+          await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+        }
+        return RedirectToAction("Index", new {Message = ManageMessageId.ChangePasswordSuccess});
+      }
+      AddErrors(result);
+      return View(model);
+    }
+
+    //
+    // GET: /Manage/SetPassword
+    public ActionResult SetPassword()
+    {
+      return View();
+    }
+
+    //
+    // POST: /Manage/SetPassword
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> SetPassword(SetPasswordViewModel model)
+    {
+      if (ModelState.IsValid)
+      {
+        var result = await UserManager.AddPasswordAsync(CurrentUserId, model.NewPassword);
         if (result.Succeeded)
         {
           var user = await UserManager.FindByIdAsync(CurrentUserId);
@@ -67,160 +125,101 @@ namespace JoinRpg.Web.Controllers
           {
             await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
           }
-          message = ManageMessageId.RemoveLoginSuccess;
+          return RedirectToAction("Index", new {Message = ManageMessageId.SetPasswordSuccess});
         }
-        else
-        {
-          message = ManageMessageId.Error;
-        }
-        return RedirectToAction("ManageLogins", new {Message = message});
+        AddErrors(result);
       }
 
-        //
-        // GET: /Manage/ChangePassword
-        public ActionResult ChangePassword()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Manage/ChangePassword
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> ChangePassword(ChangePasswordViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
-            var result = await UserManager.ChangePasswordAsync(CurrentUserId, model.OldPassword, model.NewPassword);
-            if (result.Succeeded)
-            {
-                var user = await UserManager.FindByIdAsync(CurrentUserId);
-                if (user != null)
-                {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                }
-                return RedirectToAction("Index", new { Message = ManageMessageId.ChangePasswordSuccess });
-            }
-            AddErrors(result);
-            return View(model);
-        }
-
-        //
-        // GET: /Manage/SetPassword
-        public ActionResult SetPassword()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Manage/SetPassword
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<ActionResult> SetPassword(SetPasswordViewModel model)
-        {
-            if (ModelState.IsValid)
-            {
-                var result = await UserManager.AddPasswordAsync(CurrentUserId, model.NewPassword);
-                if (result.Succeeded)
-                {
-                    var user = await UserManager.FindByIdAsync(CurrentUserId);
-                    if (user != null)
-                    {
-                        await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
-                    }
-                    return RedirectToAction("Index", new { Message = ManageMessageId.SetPasswordSuccess });
-                }
-                AddErrors(result);
-            }
-
-            // If we got this far, something failed, redisplay form
-            return View(model);
-        }
-
-        //
-        // GET: /Manage/ManageLogins
-        public async Task<ActionResult> ManageLogins(ManageMessageId? message)
-        {
-            ViewBag.StatusMessage =
-                message == ManageMessageId.RemoveLoginSuccess ? "The external login was removed."
-                : message == ManageMessageId.Error ? "An error has occurred."
-                : "";
-            var user = await UserManager.FindByIdAsync(CurrentUserId);
-            if (user == null)
-            {
-                return View("Error");
-            }
-            var userLogins = await UserManager.GetLoginsAsync(CurrentUserId);
-            var otherLogins = AuthenticationManager.GetExternalAuthenticationTypes().Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider)).ToList();
-            ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
-            return View(new ManageLoginsViewModel
-            {
-                CurrentLogins = userLogins,
-                OtherLogins = otherLogins
-            });
-        }
-
-        //
-        // POST: /Manage/LinkLogin
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult LinkLogin(string provider)
-        {
-            // Request a redirect to the external login provider to link a login for the current user
-            return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"), User.Identity.GetUserId());
-        }
-
-        //
-        // GET: /Manage/LinkLoginCallback
-        public async Task<ActionResult> LinkLoginCallback()
-        {
-            var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
-            if (loginInfo == null)
-            {
-                return RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
-            }
-            var result = await UserManager.AddLoginAsync(CurrentUserId, loginInfo.Login);
-            return result.Succeeded ? RedirectToAction("ManageLogins") : RedirectToAction("ManageLogins", new { Message = ManageMessageId.Error });
-        }
-
-
-
-#region Helpers
-        // Used for XSRF protection when adding external logins
-        private const string XsrfKey = "XsrfId";
-
-        private IAuthenticationManager AuthenticationManager
-        {
-            get
-            {
-                return HttpContext.GetOwinContext().Authentication;
-            }
-        }
-
-        private void AddErrors(IdentityResult result)
-        {
-            foreach (var error in result.Errors)
-            {
-                ModelState.AddModelError("", error);
-            }
-        }
-
-        private bool HasPassword()
-        {
-            var user = UserManager.FindById(CurrentUserId);
-          return user?.PasswordHash != null;
-        }
-
-      public enum ManageMessageId
-        {
-            ChangePasswordSuccess,
-            SetPasswordSuccess,
-            RemoveLoginSuccess,
-            Error
-        }
-
-#endregion
+      // If we got this far, something failed, redisplay form
+      return View(model);
     }
+
+    //
+    // GET: /Manage/ManageLogins
+    public async Task<ActionResult> ManageLogins(ManageMessageId? message)
+    {
+      ViewBag.StatusMessage =
+        message == ManageMessageId.RemoveLoginSuccess
+          ? "The external login was removed."
+          : message == ManageMessageId.Error
+            ? "An error has occurred."
+            : "";
+      var user = await UserManager.FindByIdAsync(CurrentUserId);
+      if (user == null)
+      {
+        return View("Error");
+      }
+      var userLogins = await UserManager.GetLoginsAsync(CurrentUserId);
+      var otherLogins =
+        AuthenticationManager.GetExternalAuthenticationTypes()
+          .Where(auth => userLogins.All(ul => auth.AuthenticationType != ul.LoginProvider))
+          .ToList();
+      ViewBag.ShowRemoveButton = user.PasswordHash != null || userLogins.Count > 1;
+      return View(new ManageLoginsViewModel
+      {
+        CurrentLogins = userLogins,
+        OtherLogins = otherLogins
+      });
+    }
+
+    //
+    // POST: /Manage/LinkLogin
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult LinkLogin(string provider)
+    {
+      // Request a redirect to the external login provider to link a login for the current user
+      return new AccountController.ChallengeResult(provider, Url.Action("LinkLoginCallback", "Manage"),
+        User.Identity.GetUserId());
+    }
+
+    //
+    // GET: /Manage/LinkLoginCallback
+    public async Task<ActionResult> LinkLoginCallback()
+    {
+      var loginInfo = await AuthenticationManager.GetExternalLoginInfoAsync(XsrfKey, User.Identity.GetUserId());
+      if (loginInfo == null)
+      {
+        return RedirectToAction("ManageLogins", new {Message = ManageMessageId.Error});
+      }
+      var result = await UserManager.AddLoginAsync(CurrentUserId, loginInfo.Login);
+      return result.Succeeded
+        ? RedirectToAction("ManageLogins")
+        : RedirectToAction("ManageLogins", new {Message = ManageMessageId.Error});
+    }
+
+
+
+    #region Helpers
+
+    // Used for XSRF protection when adding external logins
+    private const string XsrfKey = "XsrfId";
+
+    private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+    private void AddErrors(IdentityResult result)
+    {
+      foreach (var error in result.Errors)
+      {
+        ModelState.AddModelError("", error);
+      }
+    }
+
+    private bool HasPassword()
+    {
+      var user = UserManager.FindById(CurrentUserId);
+      return user?.PasswordHash != null;
+    }
+
+    public enum ManageMessageId
+    {
+      ChangePasswordSuccess,
+      SetPasswordSuccess,
+      RemoveLoginSuccess,
+      Error
+    }
+
+    #endregion
+  }
+
 }
