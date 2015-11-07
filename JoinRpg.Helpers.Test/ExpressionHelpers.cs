17921e0d806020be958ccb786a98c0d54fef5ec7
Leonid Tsarev <leo@bastilia.ru>
2015-11-07 15:06:53 +0300

    Tests for AsPropertyName

---
+++ JoinRpg.Helpers.Test/ExpressionHelpers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq.Expressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JoinRpg.Helpers.Test
+{
+  [TestClass]
+  public class ExpressionHelpers
+  {
+    // ReSharper disable once ClassNeverInstantiated.Local
+    private class AnonClass
+    {
+      // ReSharper disable once UnusedAutoPropertyAccessor.Local
+      public int Prop { get; set; }
+    }
+
+    [TestMethod]
+    public void TestAsPropertyName()
+    {
+      Expression<Func<AnonClass,int>> lambda = foo => foo.Prop;
+
+      Assert.AreEqual("Prop", lambda.AsPropertyName());
+    }
+  }
+}
