d030f917eabdc8e72f8be6044f231e2110a2c846
Leonid Tsarev <leo@bastilia.ru>
2015-12-04 13:04:00 +0300

    Add help to profile page

---
+++ Joinrpg/App_Code/MvcHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace JoinRpg.Web.App_Code
+{
+  public static class MvcHtmlHelpers
+  {
+    public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> self, Expression<Func<TModel, TValue>> expression)
+    {
+      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+      var description = metadata.Description;
+
+      if (string.IsNullOrWhiteSpace(description))
+      {
+        return MvcHtmlString.Empty;
+      }
+
+      return
+        // ReSharper disable once UseStringInterpolation we are inside Razor
+        MvcHtmlString.Create(string.Format(@"<span class=""input-group-addon""><span class=""glyphicon glyphicon-info-sign"" title=""{0}""></span></span>",
+          description));
+    }
+  }
+}
