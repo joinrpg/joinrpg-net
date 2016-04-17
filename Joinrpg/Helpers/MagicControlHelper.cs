0de6c8d5bffc5a9ed25711a93e70260ca28f3e5c
Leonid Tsarev <leo@bastilia.ru>
2016-04-17 13:05:17 +0300

    forgot MagicControlHelper

---
+++ Joinrpg/Helpers/MagicControlHelper.cs
@@ -0,0 +1,93 @@
+﻿using JoinRpg.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace JoinRpg.Web.Helpers
+{
+  public enum ShowImplicitGroups
+  {
+    None, Children, Parents
+  }
+
+  public enum MagicControlStrategy
+  {
+    Changer, NonChanger
+  }
+
+
+  public static class MagicControlHelper
+  {
+
+
+    private static string GetImplicitGroupString(ShowImplicitGroups showGroups)
+    {
+      string implicitGroupsString;
+      switch (showGroups)
+      {
+        case ShowImplicitGroups.None:
+          implicitGroupsString = "'none'";
+          break;
+        case ShowImplicitGroups.Children:
+          implicitGroupsString = "'children'";
+          break;
+        case ShowImplicitGroups.Parents:
+          implicitGroupsString = "'parents'";
+          break;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(showGroups), showGroups, null);
+      }
+
+      return implicitGroupsString;
+    }
+
+    public static MvcHtmlString GetMagicSelect(int projectId, int rootGroupId, bool showCharacters, ShowImplicitGroups showGroups, MagicControlStrategy strategy, IEnumerable<int> groupIds, string propertyName, IEnumerable<string> elements)
+    {
+      var implicitGroupsString = GetImplicitGroupString(showGroups);
+
+      var strategyString = GetStrategyString(strategy);
+
+      //TODO: convert to verbatim
+      var magicSelectFor = MvcHtmlString.Create(string.Format(@"
+<div id=""{0}_control"" style=""max-width: 700px;width:100%""></div>
+<script type=""text/javascript"">
+        $(function() {{
+        var options = 
+          {{
+            url: '/' + {1} + '/roles/' + {2} + '/indexjson',
+            multiselect: true,
+            showcharacters: {3},
+            hiddenselect: {{ id: '{0}', name: '{0}' }},
+            implicitgroups: {4},
+            strategy: {{
+              type: '{5}',
+              elements: [{6}]
+            }},
+          }};
+      var c = $('#{0}_control');
+      c.multicontrol(options);
+    }});
+</script>", propertyName, projectId, rootGroupId, showCharacters ? "true" : "false", implicitGroupsString, strategyString, elements.Join(", ")));
+      return magicSelectFor;
+    }
+
+    private static string GetStrategyString(MagicControlStrategy strategy)
+    {
+      string strategyString;
+      switch (strategy)
+      {
+        case MagicControlStrategy.Changer:
+          strategyString
+            = "changer";
+          break;
+        case MagicControlStrategy.NonChanger:
+          strategyString
+            = "nonchanger";
+          break;
+        default:
+          throw new ArgumentOutOfRangeException(nameof(strategy), strategy, null);
+      }
+      return strategyString;
+    }
+  }
+}
