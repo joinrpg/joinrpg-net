59af08f1dfc0fa0fb38df847b9333f4dc9b6d216
Kirillkos91 <kirillkos91@gmail.com>
2017-02-19 20:54:22 +0300

    Added a TargetedSearchResultViewModel and started using it to display each single search result.

---
+++ Joinrpg/Models/TargetedSearchResultViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using JetBrains.Annotations;
+using JoinRpg.Services.Interfaces.Search;
+
+namespace JoinRpg.Web.Models
+{
+  /// <summary>
+  /// A view model for a single item from search result
+  /// carrying, in additon, the target of the made search.
+  /// </summary>
+  public class TargetedSearchResultViewModel
+  { 
+    public string SearchTarget { get; private set; }
+    public ISearchResult SearchResult { get; private set; }
+
+    public TargetedSearchResultViewModel([NotNull] ISearchResult searchResult, [NotNull] string searchTarget)
+    {
+      if (searchResult == null)
+      {
+        throw new ArgumentNullException(nameof(searchResult));
+      }
+      if (searchTarget == null)
+      {
+        throw new ArgumentNullException(nameof(searchTarget));
+      }
+      SearchResult = searchResult;
+      SearchTarget = searchTarget;
+    }
+  }
+}
