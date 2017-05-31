5c4e88a3ef84ab5d1f7d573f91efe8f9e639899a
Leonid Tsarev <leo@bastilia.ru>
2017-06-01 01:14:37 +0300

    add descripton for acls

--- Joinrpg/App_Code/MvcHelpers.cs
+++ Joinrpg/App_Code/MvcHelpers.cs
@@ -8,84 +8,92 @@ using JoinRpg.Web.Models;
 
 namespace JoinRpg.Web.App_Code
 {
-  public static class MvcHtmlHelpers
-  {
-    public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> self,
-      Expression<Func<TModel, TValue>> expression)
+    public static class MvcHtmlHelpers
     {
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-      var description = metadata.Description;
+        public static string GetDescription<TModel, TValue>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, TValue>> expression)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+            return metadata.Description;
+        }
 
-      if (string.IsNullOrWhiteSpace(description))
-      {
-        return MvcHtmlString.Empty;
-      }
+        public static MvcHtmlString DescriptionFor<TModel, TValue>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, TValue>> expression)
+        {
+            var description = self.GetDescription(expression);
 
-      // ReSharper disable once UseStringInterpolation we are inside Razor
-      return MvcHtmlString.Create(string.Format(@"<div class=""help-block"">{0}</div>", description));
+            if (string.IsNullOrWhiteSpace(description))
+            {
+                return MvcHtmlString.Empty;
+            }
+
+            // ReSharper disable once UseStringInterpolation we are inside Razor
+            return MvcHtmlString.Create(string.Format(@"<div class=""help-block"">{0}</div>", description));
+        }
+
+        public static TValue GetValue<TModel, TValue>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, TValue>> expression)
+        {
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+            return (TValue) metadata.Model;
+        }
+
+        public static TModel GetModel<TModel>(this HtmlHelper<TModel> self)
+        {
+            return (TModel) ModelMetadata.FromLambdaExpression(m => m, self.ViewData).Model;
+        }
+
+        public static MvcHtmlString MagicSelectParent<TModel>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, IEnumerable<string>>> expression)
+            where TModel : IProjectIdAware
+        {
+            var container = (IProjectIdAware) self.GetModel();
+
+            var value = self.GetValue(expression).ToList();
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+
+            return MagicControlHelper.GetMagicSelect(container.ProjectId, false,
+                ShowImplicitGroups.Parents, MagicControlStrategy.NonChanger, metadata.PropertyName, value, false);
+        }
+
+        public static MvcHtmlString MagicSelectBindGroups<TModel>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, IEnumerable<string>>> expression)
+            where TModel : IProjectIdAware
+        {
+            var container = (IProjectIdAware) self.GetModel();
+
+            var value = self.GetValue(expression).ToList();
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+
+            return MagicControlHelper.GetMagicSelect(container.ProjectId, false,
+                ShowImplicitGroups.Children, MagicControlStrategy.NonChanger, metadata.PropertyName, value, true);
+        }
+
+        public static MvcHtmlString MagicSelectGroupParent<TModel>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, IEnumerable<string>>> expression)
+            where TModel : EditCharacterGroupViewModel
+        {
+            var container = (EditCharacterGroupViewModel) self.GetModel();
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+
+            return MagicControlHelper.GetMagicSelect(container.ProjectId, false, ShowImplicitGroups.Parents,
+                MagicControlStrategy.Changer, metadata.PropertyName, container.CharacterGroupId.PrefixAsGroups(),
+                false);
+        }
+
+        public static MvcHtmlString MagicSelectBind<TModel>(this HtmlHelper<TModel> self,
+            Expression<Func<TModel, IEnumerable<string>>> expression)
+            where TModel : IProjectIdAware
+        {
+            var container = self.GetModel();
+
+            var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
+
+            var value = self.GetValue(expression);
+
+            return MagicControlHelper.GetMagicSelect(container.ProjectId, true, ShowImplicitGroups.Children,
+                MagicControlStrategy.NonChanger, metadata.PropertyName, value, true);
+        }
     }
-
-    public static TValue GetValue<TModel, TValue>(this HtmlHelper<TModel> self, Expression<Func<TModel, TValue>> expression)
-    {
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-      return (TValue)metadata.Model;
-    }
-
-    public static TModel GetModel<TModel>(this HtmlHelper<TModel> self)
-    {
-      return (TModel) ModelMetadata.FromLambdaExpression(m => m, self.ViewData).Model;
-    }
-
-    public static MvcHtmlString MagicSelectParent<TModel>(this HtmlHelper<TModel> self,
-  Expression<Func<TModel, IEnumerable<string>>> expression)
-  where TModel : IProjectIdAware
-    {
-      var container = (IProjectIdAware)self.GetModel();
-
-      var value = self.GetValue(expression).ToList();
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-
-      return MagicControlHelper.GetMagicSelect(container.ProjectId, false,
-        ShowImplicitGroups.Parents, MagicControlStrategy.NonChanger, metadata.PropertyName, value, false);
-    }
-
-    public static MvcHtmlString MagicSelectBindGroups<TModel>(this HtmlHelper<TModel> self,
-      Expression<Func<TModel, IEnumerable<string>>> expression)
-      where TModel : IProjectIdAware
-    {
-      var container = (IProjectIdAware) self.GetModel();
-
-      var value = self.GetValue(expression).ToList();
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-
-      return MagicControlHelper.GetMagicSelect(container.ProjectId, false,
-        ShowImplicitGroups.Children, MagicControlStrategy.NonChanger, metadata.PropertyName, value, true);
-    }
-
-    public static MvcHtmlString MagicSelectGroupParent<TModel>(this HtmlHelper<TModel> self,
-  Expression<Func<TModel, IEnumerable<string>>> expression)
-  where TModel : EditCharacterGroupViewModel
-    {
-      var container =(EditCharacterGroupViewModel)  self.GetModel();
-
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-
-      return MagicControlHelper.GetMagicSelect(container.ProjectId, false, ShowImplicitGroups.Parents,
-        MagicControlStrategy.Changer, metadata.PropertyName, container.CharacterGroupId.PrefixAsGroups(), false);
-    }
-
-    public static MvcHtmlString MagicSelectBind<TModel>(this HtmlHelper<TModel> self,
-Expression<Func<TModel, IEnumerable<string>>> expression)
-where TModel : IProjectIdAware
-    {
-      var container = self.GetModel();
-
-      var metadata = ModelMetadata.FromLambdaExpression(expression, self.ViewData);
-
-      var value = self.GetValue(expression);
-
-      return MagicControlHelper.GetMagicSelect(container.ProjectId, true, ShowImplicitGroups.Children,
-        MagicControlStrategy.NonChanger, metadata.PropertyName, value, true);
-    }
-  }
 }
