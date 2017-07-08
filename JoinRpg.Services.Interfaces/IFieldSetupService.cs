282a0931d77b11c0dbf2a711524442b7292b6094
Leonid Tsarev <leo@bastilia.ru>
2017-07-08 19:18:34 +0300

    Create/update for special groups

--- JoinRpg.Services.Interfaces/IFieldSetupService.cs
+++ JoinRpg.Services.Interfaces/IFieldSetupService.cs
@@ -7,21 +7,21 @@ namespace JoinRpg.Services.Interfaces
 {
   public interface IFieldSetupService
   {
-    Task UpdateFieldParams(int? currentUserId, int projectId, int fieldId, string name, string fieldHint,
+    Task UpdateFieldParams(int projectId, int fieldId, string name, string fieldHint,
       bool canPlayerEdit, bool canPlayerView, bool isPublic, MandatoryStatus mandatoryStatus, List<int> showForGroups,
       bool validForNpc, bool includeInPrint, bool showForUnapprovedClaims);
     Task DeleteField(int projectId, int projectFieldId);
 
-    Task AddField(int projectId, int currentUserId, ProjectFieldType fieldType, string name, string fieldHint,
+    Task AddField(int projectId, ProjectFieldType fieldType, string name, string fieldHint,
       bool canPlayerEdit, bool canPlayerView, bool isPublic, FieldBoundTo fieldBoundTo, MandatoryStatus mandatoryStatus,
       List<int> showForGroups, bool validForNpc, bool includeInPrint, bool showForUnapprovedClaims);
 
     Task CreateFieldValueVariant(int projectId, int projectCharacterFieldId, string label,
       string description);
 
-    Task UpdateFieldValueVariant(int projectId, int projectFieldDropdownValueId, int currentUserId, string label, string description, int projectFieldId);
+    Task UpdateFieldValueVariant(int projectId, int projectFieldDropdownValueId, string label, string description, int projectFieldId);
 
-    Task DeleteFieldValueVariant(int projectId, int projectFieldDropdownValueId, int currentUserId, int projectFieldId);
+    Task DeleteFieldValueVariant(int projectId, int projectFieldDropdownValueId, int projectFieldId);
     Task MoveField(int projectid, int projectcharacterfieldid, short direction);
     Task MoveFieldValue(int projectid, int projectFieldId, int projectFieldVariantId, short direction);
 
