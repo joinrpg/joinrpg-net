06826298f523eb126c872e004b62c8d3270da216
Leonid Tsarev <leo@bastilia.ru>
2017-07-18 22:08:38 +0300

    Add way to get projects I have access to

---
+++ Joinrpg/Controllers/XGameApi/MyProfileController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using JoinRpg.Data.Interfaces;
+
+namespace JoinRpg.Web.Controllers.XGameApi
+{
+  [RoutePrefix("x-api/me")]
+  public class MyProfileController : XGameApiController
+  {
+    public MyProfileController(IProjectRepository projectRepository) : base(projectRepository)
+    {
+    }
+
+    [HttpGet, Authorize, Route("projects/active")]
+    public async Task<object> GetActiveProjects()
+    {
+      return (await ProjectRepository.GetMyActiveProjectsAsync(GetCurrentUserId())).Select(p => new
+      {
+        p.ProjectId,
+        p.ProjectName
+      });
+    }
+  }
+}
