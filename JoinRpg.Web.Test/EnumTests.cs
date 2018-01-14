8902cf16ec0391bb9663a3b5be7b263bb5eacf25
Leonid Tsarev <leo@bastilia.ru>
2018-01-14 12:54:41 +0300

    fix build after merge

--- JoinRpg.Web.Test/EnumTests.cs
+++ JoinRpg.Web.Test/EnumTests.cs
@@ -7,14 +7,15 @@ using Xunit;
 namespace JoinRpg.Web.Test
 {
 
-  public class EnumTests
-  {
-    [Fact]
-    public void ProblemEnum()
+    public class EnumTests
     {
-      EnumerationTestHelper.CheckEnums<UserExtensions.AccessReason, AccessReason>();
-      EnumerationTestHelper.CheckEnums<ProjectFieldViewType, DataModel.ProjectFieldType>();
-      EnumerationTestHelper.CheckEnums<ClaimStatusView, DataModel.Claim.Status>();
-      EnumerationTestHelper.CheckEnums<FinanceOperationActionView, FinanceOperationAction>();
+        [Fact]
+        public void ProblemEnum()
+        {
+            EnumerationTestHelper.CheckEnums<UserExtensions.AccessReason, AccessReason>();
+            EnumerationTestHelper.CheckEnums<ProjectFieldViewType, DataModel.ProjectFieldType>();
+            EnumerationTestHelper.CheckEnums<ClaimStatusView, DataModel.Claim.Status>();
+            EnumerationTestHelper.CheckEnums<FinanceOperationActionView, FinanceOperationAction>();
+        }
     }
 }
