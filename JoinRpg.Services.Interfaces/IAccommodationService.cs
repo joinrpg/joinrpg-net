86584034d058c8d82aaba09308a2070060546b78
Vladislav Kiselev <vlad.kiselev@swissmicrosystems.com>
2018-01-27 13:02:49 +0300

    Parsing rooms lists and ranges inside AccommodationService.RegisterNewProjectAccommodationAsync (like 5,6,10-12,13,16-33)

--- JoinRpg.Services.Interfaces/IAccommodationService.cs
+++ JoinRpg.Services.Interfaces/IAccommodationService.cs
@@ -7,12 +7,24 @@ namespace JoinRpg.Services.Interfaces
 {
     public interface IAccommodationService
     {
+        /// <summary>
+        /// Adds new room type
+        /// </summary>
         Task<ProjectAccommodationType> RegisterNewAccommodationTypeAsync(ProjectAccommodationType newAccommodation);
-        Task<ProjectAccommodation> RegisterNewProjectAccommodationAsync(ProjectAccommodation newProjectAccommodation);
+
+        /// <summary>
+        /// Adds new room or list of rooms
+        /// </summary>
+        Task<IEnumerable<ProjectAccommodation>> RegisterNewProjectAccommodationAsync(ProjectAccommodation newProjectAccommodation);
+
         Task RemoveAccommodationType(int accommodationTypeId);
+
         Task RemoveProjectAccommodation(int projectAccommodationId);
+
         Task<IReadOnlyCollection<ProjectAccommodationType>> GetAccommodationForProject(int project);
+
         Task<ProjectAccommodationType> GetAccommodationByIdAsync(int accId);
+
         Task<ProjectAccommodation> GetProjectAccommodationByIdAsync(int accId);
 
     }
