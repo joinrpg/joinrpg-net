82832d237d5b0b6eab8f679a0e2150a8247ce4cd
Leonid Tsarev <leo@bastilia.ru>
2018-01-28 16:22:45 +0300

    UnOccupyRoomType & UnOccupyAll

--- JoinRpg.Services.Impl/AccommodationServiceImpl.cs
+++ JoinRpg.Services.Impl/AccommodationServiceImpl.cs
@@ -142,25 +142,46 @@ namespace JoinRpg.Services.Impl
 
         public async Task UnOccupyRoomAll(UnOccupyAllRequest request)
         {
-            var room = await UnitOfWork.GetDbSet<ProjectAccommodation>()
-                .Include(r => r.Project)
-                .Include(r => r.Inhabitants)
-                .Include(r => r.ProjectAccommodationType)
-                .Include(r => r.Inhabitants.Select(i => i.Subjects.Select(c => c.Player)))
-                .Where(r => r.ProjectId == request.ProjectId && r.Id == request.RoomId)
+            var room = await GetRoomQuery(request.ProjectId)
+                .Where(r => r.Id == request.RoomId)
                 .FirstOrDefaultAsync();
 
             await UnOccupyRoomImpl(room, room.Inhabitants.ToList());
         }
 
-        public Task UnOccupyRoomType(UnOccupyRoomTypeRequest request)
+        private IQueryable<ProjectAccommodation> GetRoomQuery(int projectId)
         {
-            throw new NotImplementedException();
+            return UnitOfWork.GetDbSet<ProjectAccommodation>()
+                .Include(r => r.Project)
+                .Include(r => r.Inhabitants)
+                .Include(r => r.ProjectAccommodationType)
+                .Include(r => r.Inhabitants.Select(i => i.Subjects.Select(c => c.Player)))
+                .Where(r => r.ProjectId == projectId);
         }
 
-        public Task UnOccupyAll(int projectId)
+        public async Task UnOccupyRoomType(UnOccupyRoomTypeRequest request)
         {
-            throw new NotImplementedException();
+            var rooms = await GetRoomQuery(request.ProjectId)
+                .Where(r => r.Inhabitants.Any())
+                .Where(r => r.AccommodationTypeId == request.RoomTypeId)
+                .ToListAsync();
+
+            foreach (var room in rooms)
+            {
+                await UnOccupyRoomImpl(room, room.Inhabitants.ToList());
+            }
+        }
+
+        public async Task UnOccupyAll(int projectId)
+        {
+            var rooms = await GetRoomQuery(projectId)
+                .Where(r => r.Inhabitants.Any())
+                .ToListAsync();
+
+            foreach (var room in rooms)
+            {
+                await UnOccupyRoomImpl(room, room.Inhabitants.ToList());
+            }
         }
 
         public async Task RemoveRoomType(int accomodationTypeId)
