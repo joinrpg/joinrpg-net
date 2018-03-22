328b1e0d0888df7ec944c4d706a246c3ae8bddfd
Leonid Tsarev <leo@bastilia.ru>
2018-03-22 22:38:07 +0300

    improve shouldly helpers

--- JoinRpg.Web.Test/ShouldlyFieldValueViewModelExtensions.cs
+++ JoinRpg.Web.Test/ShouldlyFieldValueViewModelExtensions.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using JoinRpg.Web.Models;
 using Shouldly;
 
@@ -5,18 +6,27 @@ namespace JoinRpg.Web.Test
 {
     internal static class ShouldlyFieldValueViewModelExtensions
     {
+        //Attributes in this class will cause debugger to stop on exception in caller of that method
+        //and thats probably what we like to see
+
+
+        [DebuggerStepThrough]
         public static void ShouldNotHaveValue(this FieldValueViewModel field) =>
             field.Value.ShouldBeNull();
 
+        [DebuggerStepThrough]
         public static void ShouldBeEditable(this FieldValueViewModel field) =>
             field.CanEdit.ShouldBeTrue();
 
+        [DebuggerStepThrough]
         public static void ShouldBeHidden(this FieldValueViewModel field) =>
             field.CanView.ShouldBeFalse();
 
+        [DebuggerStepThrough]
         public static void ShouldBeReadonly(this FieldValueViewModel field) =>
             field.CanEdit.ShouldBeFalse();
 
+        [DebuggerStepThrough]
         public static void ShouldBeVisible(this FieldValueViewModel field) =>
             field.CanView.ShouldBeTrue();
     }
