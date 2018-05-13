2d29eb9c4ccc4b2014ee94aefe35057acde52095
Leonid Tsarev <leo@bastilia.ru>
2018-05-13 14:13:05 +0300

    Add skipped unit test for yandex media

---
+++ JoinRpg.Markdown.Test/MarkdownExtraGoodiesTest.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace JoinRpg.Markdown.Test
+{
+    public class MarkdownExtraGoodiesTest
+    {
+
+        [Fact]
+        public void RenderYandex()
+        {
+            "![Audio4](https://music.yandex.ru/album/411845/track/4402274)"
+                .ShouldBeHtml("<p><iframe src=\"https://music.yandex.ru/iframe/#track/4402274/411845/\" width=\"500\" height=\"281\" frameborder=\"0\"></iframe></p>");
+        }
+    }
+}
