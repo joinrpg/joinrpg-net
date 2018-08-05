b3361edc2eadca2c44c238b7941e8d9bac8a1df0
Leonid Tsarev <leo@bastilia.ru>
2018-08-05 15:14:46 +0300

    show all groups in character api

--- JoinRpg.Data.Interfaces/ICharacterRepository.cs
+++ JoinRpg.Data.Interfaces/ICharacterRepository.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
@@ -35,16 +35,28 @@ namespace JoinRpg.Data.Interfaces
     public bool IsAcceptingClaims { get; set; }
     public ClaimView ApprovedClaim { get; set; }
     public IReadOnlyCollection<ClaimHeader> Claims { get; set; }
-    public IReadOnlyCollection<GroupHeader> Groups { get; set; }
-    public string JsonData { get; set; }
+    public IReadOnlyCollection<GroupHeader> DirectGroups { get; set; }
+      public IReadOnlyCollection<GroupHeader> AllGroups { get; set; }
+        public string JsonData { get; set; }
   }
 
-  public class GroupHeader
+  public class GroupHeader : IEquatable<GroupHeader>
   {
     public bool IsActive { get; set; }
     public bool IsSpecial { get; set; }
     public int CharacterGroupId { get; set; }
     public string CharacterGroupName { get; set; }
+
+      public IntList ParentGroupIds { get; set; }
+
+      /// <inheritdoc />
+      public bool Equals(GroupHeader other) => other != null && CharacterGroupId == other.CharacterGroupId;
+
+      /// <inheritdoc />
+      public override bool Equals(object obj) => Equals(obj as GroupHeader);
+
+      /// <inheritdoc />
+      public override int GetHashCode() => CharacterGroupId;
   }
 
   public class ClaimView : IFieldContainter
--- JoinRpg.Web.XGameApi.Contract/CharacterInfo.cs
+++ JoinRpg.Web.XGameApi.Contract/CharacterInfo.cs
@@ -1,68 +1,83 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace JoinRpg.Web.XGameApi.Contract
 {
-  /// <summary>
-  /// Full character info
-  /// </summary>
-  public class CharacterInfo
-  {
     /// <summary>
-    /// Id
+    /// Full character info
     /// </summary>
-    public int CharacterId { get; set; }
-    /// <summary>
-    /// Last modified (UTC)
-    /// </summary>
-    public DateTime UpdatedAt { get; set; }
-    /// <summary>
-    /// Active /deleted
-    /// </summary>
-    public bool IsActive { get; set; }
-    /// <summary>
-    /// True, if character in game (player checked in and not checked out)
-    /// </summary>
-    public bool InGame { get; set; }
+    public class CharacterInfo
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public int CharacterId { get; set; }
+
+        /// <summary>
+        /// Last modified (UTC)
+        /// </summary>
+        public DateTime UpdatedAt { get; set; }
+
+        /// <summary>
+        /// Active /deleted
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// True, if character in game (player checked in and not checked out)
+        /// </summary>
+        public bool InGame { get; set; }
+
+        /// <summary>
+        /// Has player or not
+        /// </summary>
+        public CharacterBusyStatus BusyStatus { get; set; }
+
+        /// <summary>
+        /// Groups that character part of (directly)
+        /// </summary>
+        public IOrderedEnumerable<GroupHeader> Groups { get; set; }
+
+        /// <summary>
+        /// Groups that character part of 
+        /// </summary>
+        public IOrderedEnumerable<GroupHeader> AllGroups { get; set; }
+
+        /// <summary>
+        /// Field values
+        /// </summary>
+        public IEnumerable<FieldValue> Fields { get; set; }
+
+        /// <summary>
+        /// Player user id
+        /// </summary>
+        public int? PlayerUserId { get; set; }
+    }
+
     /// <summary>
     /// Has player or not
     /// </summary>
-    public CharacterBusyStatus BusyStatus { get; set; }
-    /// <summary>
-    /// Groups that character part of 
-    /// </summary>
-    public IOrderedEnumerable<GroupHeader> Groups { get; set; }
-    /// <summary>
-    /// Field values
-    /// </summary>
-    public IEnumerable<FieldValue> Fields { get; set; }
-    /// <summary>
-    /// Player user id
-    /// </summary>
-    public int? PlayerUserId { get; set; }
-  }
+    public enum CharacterBusyStatus
+    {
+        /// <summary>
+        /// Has player
+        /// </summary>
+        HasPlayer,
 
-  /// <summary>
-  /// Has player or not
-  /// </summary>
-  public enum CharacterBusyStatus
-  {
-    /// <summary>
-    /// Has player
-    /// </summary>
-    HasPlayer,
-    /// <summary>
-    /// Has some claims, but nothing approved
-    /// </summary>
-    Discussed,
-    /// <summary>
-    /// No actve claims
-    /// </summary>
-    NoClaims,
-    /// <summary>
-    /// NPC should not have any claims
-    /// </summary>
-    Npc,
-  }
+        /// <summary>
+        /// Has some claims, but nothing approved
+        /// </summary>
+        Discussed,
+
+        /// <summary>
+        /// No actve claims
+        /// </summary>
+        NoClaims,
+
+        /// <summary>
+        /// NPC should not have any claims
+        /// </summary>
+        Npc,
+    }
 }
