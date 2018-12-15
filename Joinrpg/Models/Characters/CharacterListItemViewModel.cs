e4ab44f89ed21c500d2606c07c3a2ab4d4c6b74e
Leonid Tsarev <leo@bastilia.ru>
2018-12-15 16:07:58 +0300

    Add responsible master to excel export

--- Joinrpg/Models/Characters/CharacterListItemViewModel.cs
+++ Joinrpg/Models/Characters/CharacterListItemViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
@@ -11,104 +11,122 @@ using JoinRpg.Web.Models.CharacterGroups;
 
 namespace JoinRpg.Web.Models.Characters
 {
-  public class CharacterListByGroupViewModel : CharacterListViewModel
-  {
-    public CharacterListByGroupViewModel(int currentUserId, IReadOnlyCollection<Character> characters, CharacterGroup group)
-      : base(currentUserId, $"Персонажи — {group.CharacterGroupName}", characters, group.Project)
+    public class CharacterListByGroupViewModel : CharacterListViewModel
     {
-      GroupModel = new CharacterGroupDetailsViewModel(group, currentUserId, GroupNavigationPage.Characters);
+        public CharacterListByGroupViewModel(int currentUserId,
+            IReadOnlyCollection<Character> characters,
+            CharacterGroup group)
+            : base(currentUserId,
+                $"Персонажи — {group.CharacterGroupName}",
+                characters,
+                group.Project)
+        {
+            GroupModel =
+                new CharacterGroupDetailsViewModel(group,
+                    currentUserId,
+                    GroupNavigationPage.Characters);
+        }
+
+        public CharacterGroupDetailsViewModel GroupModel { get; }
     }
 
-    public CharacterGroupDetailsViewModel GroupModel { get; }
-  }
-
-  public class CharacterListViewModel : IOperationsAwareView
-  {
-    public IEnumerable<CharacterListItemViewModel> Items { get; }
-    public int? ProjectId { get; }
-    public IReadOnlyCollection<int> ClaimIds { get; }
-    public IReadOnlyCollection<int> CharacterIds { get; }
-    public string ProjectName { get; }
-    public string Title { get; }
-
-    public bool HasEditAccess { get; }
-
-    public CharacterListViewModel(
-      int currentUserId, 
-      string title, 
-      IReadOnlyCollection<Character> characters, 
-      Project project)
+    public class CharacterListViewModel : IOperationsAwareView
     {
-      Items = characters.Select(
-        character =>
-          new CharacterListItemViewModel(character, currentUserId, character.GetProblems())).ToArray();
-      ProjectName = project.ProjectName;
-      ProjectId = project.ProjectId;
-      Title = title;
-      Fields = project.GetOrderedFields().Where(f => f.IsActive).ToArray();
-      ClaimIds = characters.Select(c => c.ApprovedClaim?.ClaimId).WhereNotNull().ToArray();
-      CharacterIds = characters.Select(c => c.CharacterId).ToArray();
-      HasEditAccess = project.HasEditRolesAccess(currentUserId);
+        public IEnumerable<CharacterListItemViewModel> Items { get; }
+        public int? ProjectId { get; }
+        public IReadOnlyCollection<int> ClaimIds { get; }
+        public IReadOnlyCollection<int> CharacterIds { get; }
+        public string ProjectName { get; }
+        public string Title { get; }
+
+        public bool HasEditAccess { get; }
+
+        public CharacterListViewModel(
+            int currentUserId,
+            string title,
+            IReadOnlyCollection<Character> characters,
+            Project project)
+        {
+            Items = characters.Select(
+                character =>
+                    new CharacterListItemViewModel(character,
+                        currentUserId,
+                        character.GetProblems())).ToArray();
+            ProjectName = project.ProjectName;
+            ProjectId = project.ProjectId;
+            Title = title;
+            Fields = project.GetOrderedFields().Where(f => f.IsActive).ToArray();
+            ClaimIds = characters.Select(c => c.ApprovedClaim?.ClaimId).WhereNotNull().ToArray();
+            CharacterIds = characters.Select(c => c.CharacterId).ToArray();
+            HasEditAccess = project.HasEditRolesAccess(currentUserId);
+        }
+
+        public IReadOnlyCollection<ProjectField> Fields { get; }
     }
 
-    public IReadOnlyCollection<ProjectField> Fields { get; }
-  }
-
-  public class CharacterListItemViewModel : ILinkable
-  {
-    [Display(Name="Занят?")]
-    public CharacterBusyStatusView BusyStatus { get; }
-
-    [Display(Name = "Персонаж")]
-    public string Name { get; set; }
-
-    public int CharacterId { get; }
-
-    [NotNull, ReadOnly(true)]
-    public IReadOnlyCollection<FieldWithValue> Fields { get; }
-
-    public int? ApprovedClaimId { get; }
-
-    [Display(Name = "Игрок"), CanBeNull]
-    public User Player { get; set; }
-
-    [ReadOnly(true), DisplayName("Входит в группы")]
-    public CharacterParentGroupsViewModel Groups { get; }
-
-    public CharacterListItemViewModel (
-      [NotNull] Character character, 
-      int currentUserId, 
-      IEnumerable<ClaimProblem> problems)
+    public class CharacterListItemViewModel : ILinkable
     {
-      if (character == null) throw new ArgumentNullException(nameof(character));
+        [Display(Name = "Занят?")]
+        public CharacterBusyStatusView BusyStatus { get; }
 
-      BusyStatus = character.GetBusyStatus();
+        [Display(Name = "Персонаж")]
+        public string Name { get; set; }
 
-      if (character.ApprovedClaim != null)
-      {
-        ApprovedClaimId = character.ApprovedClaim.ClaimId;
-        Player = character.ApprovedClaim.Player;
-      }
-      Name = character.CharacterName;
-      CharacterId = character.CharacterId;
-      ProjectId = character.ProjectId;
-      Fields = character.Project.GetFieldsNotFilledWithoutOrder().ToList();
-      Fields.FillFrom(character);
-      Fields.FillFrom(character.ApprovedClaim);
-      Problems = problems.Select(p => new ProblemViewModel(p)).ToList();
+        public int CharacterId { get; }
 
-      Groups = new CharacterParentGroupsViewModel(character, character.HasMasterAccess(currentUserId));
+        [NotNull, ReadOnly(true)]
+        public IReadOnlyCollection<FieldWithValue> Fields { get; }
+
+        public int? ApprovedClaimId { get; }
+
+        [Display(Name = "Игрок"), CanBeNull]
+        public User Player { get; set; }
+
+        [ReadOnly(true), DisplayName("Входит в группы")]
+        public CharacterParentGroupsViewModel Groups { get; }
+
+        [Display(Name = "Ответственный мастер")]
+        public User Responsible { get; }
+
+        public CharacterListItemViewModel(
+            [NotNull]
+            Character character,
+            int currentUserId,
+            IEnumerable<ClaimProblem> problems)
+        {
+            if (character == null) throw new ArgumentNullException(nameof(character));
+
+            BusyStatus = character.GetBusyStatus();
+
+            if (character.ApprovedClaim != null)
+            {
+                ApprovedClaimId = character.ApprovedClaim.ClaimId;
+                Player = character.ApprovedClaim.Player;
+            }
+
+            Name = character.CharacterName;
+            CharacterId = character.CharacterId;
+            ProjectId = character.ProjectId;
+            Fields = character.Project.GetFieldsNotFilledWithoutOrder().ToList();
+            Fields.FillFrom(character);
+            Fields.FillFrom(character.ApprovedClaim);
+            Problems = problems.Select(p => new ProblemViewModel(p)).ToList();
+
+            Groups = new CharacterParentGroupsViewModel(character,
+                character.HasMasterAccess(currentUserId));
+
+            Responsible = character.GetResponsibleMaster();
+        }
+
+        [Display(Name = "Проблемы")]
+        public ICollection<ProblemViewModel> Problems { get; set; }
+
+        #region Implementation of ILinkable
+
+        public LinkType LinkType => LinkType.ResultCharacter;
+        public string Identification => CharacterId.ToString();
+        public int? ProjectId { get; }
+
+        #endregion
     }
-
-    [Display(Name="Проблемы")]
-    public ICollection<ProblemViewModel> Problems { get; set; }
-
-    #region Implementation of ILinkable
-
-    public LinkType LinkType => LinkType.ResultCharacter;
-    public string Identification => CharacterId.ToString();
-    public int? ProjectId { get; }
-
-    #endregion
-  }
 }
--- Joinrpg/Models/Exporters/CharacterListExporterBase.cs
+++ Joinrpg/Models/Exporters/CharacterListExporterBase.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 using JoinRpg.DataModel;
 using JoinRpg.Services.Interfaces;
@@ -6,22 +6,25 @@ using JoinRpg.Web.Models.Characters;
 
 namespace JoinRpg.Web.Models.Exporters
 {
-  public abstract class CharacterListExporterBase : CustomExporter<CharacterListItemViewModel>
-  {
-    protected CharacterListExporterBase(IReadOnlyCollection<ProjectField> fields, IUriService uriService)
-      : base(uriService)
+    public abstract class CharacterListExporterBase : CustomExporter<CharacterListItemViewModel>
     {
-      Fields = fields;
-    }
-    protected IReadOnlyCollection<ProjectField> Fields { get; }
+        protected CharacterListExporterBase(IReadOnlyCollection<ProjectField> fields,
+            IUriService uriService)
+            : base(uriService)
+        {
+            Fields = fields;
+        }
 
-    protected IEnumerable<ITableColumn> BasicColumns()
-    {
-      yield return StringColumn(x => x.Name);
-      yield return UriColumn(x => x, "Персонаж");
-      yield return EnumColumn(x => x.BusyStatus);
-      yield return StringListColumn(x => x.Groups.ParentGroups.Select(g => g.Name), "Группы");
-      yield return UriListColumn(x => x.Groups.ParentGroups);
+        protected IReadOnlyCollection<ProjectField> Fields { get; }
+
+        protected IEnumerable<ITableColumn> BasicColumns()
+        {
+            yield return StringColumn(x => x.Name);
+            yield return UriColumn(x => x, "Персонаж");
+            yield return EnumColumn(x => x.BusyStatus);
+            yield return StringListColumn(x => x.Groups.ParentGroups.Select(g => g.Name), "Группы");
+            yield return UriListColumn(x => x.Groups.ParentGroups);
+            yield return ShortUserColumn(x => x.Responsible);
+        }
     }
-  }
 }
