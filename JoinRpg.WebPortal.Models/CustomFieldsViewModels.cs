7b9081cbc5896718ae6e5b9b50b497ee616d9370
Leonid Tsarev <leo@bastilia.ru>
2019-02-02 17:20:19 +0300

    remove namespaces

--- JoinRpg.WebPortal.Models/CustomFieldsViewModels.cs
+++ JoinRpg.WebPortal.Models/CustomFieldsViewModels.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
 using JetBrains.Annotations;
 using Joinrpg.Markdown;
 using JoinRpg.DataModel;
