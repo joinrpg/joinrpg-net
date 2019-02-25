70b99bf08550c18a423ed52374f8beb3814495a0
Leonid Tsarev <leo@bastilia.ru>
2019-02-25 22:45:12 +0300

    Add isLarper API (for noonesfriend)

---
+++ JoinRpg.Services.Interfaces/IAntiSpamService.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JoinRpg.Services.Interfaces
+{
+    /// <summary>
+    /// Joinrpg helps to battle spam
+    /// </summary>
+    public interface IAntiSpamService
+    {
+        /// <summary>
+        /// Is user a larper? Yes, No, DontKnow
+        /// </summary>
+        Task<bool> IsLarper(IsLarperRequest request);
+    }
+
+    /// <summary>
+    /// Request model
+    /// </summary>
+    public class IsLarperRequest
+    {
+        /// <summary>
+        /// Email for user
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Vk id (https://vk.com/id[001])
+        /// </summary>
+        public string VkId { get; set; }
+
+        /// <summary>
+        /// Vk nickname (https://vk.com/[user])
+        /// </summary>
+        public string VkNickName { get; set; }
+
+        /// <summary>
+        /// Telegram nickname @[username]
+        /// </summary>
+        public string TelegramNickName { get; set; }
+    }
+}
