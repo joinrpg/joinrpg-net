0e196568c18b12f03e813d8c04f968edc442617b
Leonid Tsarev <leo@bastilia.ru>
2019-02-27 17:32:48 +0300

    Add policies on reg and game creation

--- JoinRpg.WebPortal.Models/ProjectCreateViewModel.cs
+++ JoinRpg.WebPortal.Models/ProjectCreateViewModel.cs
@@ -1,11 +1,22 @@
-﻿using System.ComponentModel;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace JoinRpg.Web.Models
 {
-  public class ProjectCreateViewModel
-  {
-    [DisplayName("Название игры"), Required, StringLength(100, ErrorMessage = "Название игры должно быть длиной от 5 до 100 букв.", MinimumLength = 5)]
-    public string ProjectName { get; set; }
-  }
+    public class ProjectCreateViewModel
+    {
+        [DisplayName("Название игры"), Required,
+         StringLength(100,
+             ErrorMessage = "Название игры должно быть длиной от 5 до 100 букв.",
+             MinimumLength = 5)]
+        public string ProjectName { get; set; }
+
+        [Required]
+        [Display(Name = "Согласен с правилами")]
+        [Range(typeof(bool),
+            "true",
+            "true",
+            ErrorMessage = "Согласитесь с правилами, чтобы продолжить")]
+        public bool RulesApproved { get; set; }
+    }
 }
