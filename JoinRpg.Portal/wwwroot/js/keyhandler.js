fa424b8839daed9c83c57a8a57cb0e389b801905
Leonid Tsarev <leo@bastilia.ru>
2019-04-27 18:15:21 +0300

    move keyhandler

---
+++ JoinRpg.Portal/wwwroot/js/keyhandler.js
@@ -0,0 +1,23 @@
+﻿
+function emailKeyPress(ev) {
+    if (ev.charCode === 32) {
+        ev.stopPropagation();
+        return false;
+    }
+    return true;
+}
+
+function emailPaste(o) {
+    o.checkInput = true;
+}
+
+function emailInput(o) {
+    if (o.checkInput === true) {
+        o.value = cutSpaces(o.value.toString());
+        o.checkInput = false;
+    }
+}
+
+function cutSpaces(s) {
+    return s.replace(/\s+/g, "");
+}
