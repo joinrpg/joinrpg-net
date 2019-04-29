3bb45032b635ee56de29b1597a6e82598793ddc3
Leonid Tsarev <leo@bastilia.ru>
2019-04-29 10:44:52 +0300

    inject db context configuration via container

--- JoinRpg.Portal/Infrastructure/ConfigurationAdapter.cs
+++ JoinRpg.Portal/Infrastructure/ConfigurationAdapter.cs
@@ -1,13 +1,21 @@
 using System;
+using JoinRpg.Dal.Impl;
 using JoinRpg.Services.Interfaces;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 
 namespace JoinRpg.Portal.Infrastructure
 {
-    public class ConfigurationAdapter : IMailGunConfig
+    /// <summary>
+    /// Read configuration
+    /// </summary>
+    public class ConfigurationAdapter : IMailGunConfig, IJoinDbContextConfiguration
     {
         private readonly IConfiguration configuration;
 
+        /// <summary>
+        /// ctor
+        /// </summary>
         public ConfigurationAdapter(IConfiguration configuration)
         {
             this.configuration = configuration;
@@ -18,5 +26,9 @@ namespace JoinRpg.Portal.Infrastructure
         string IMailGunConfig.ApiKey => configuration.GetValue<string>("MailGunApiKey");
 
         string IMailGunConfig.ServiceEmail => "support@" + configuration.GetValue<string>("MailGunApiDomain");
+
+        string IJoinDbContextConfiguration.ConnectionString
+            => configuration.GetConnectionString("DefaultConnection")
+            .Replace("!!!!", configuration.GetValue<string>(WebHostDefaults.ContentRootKey)+"\\App_Data");
     }
 }
