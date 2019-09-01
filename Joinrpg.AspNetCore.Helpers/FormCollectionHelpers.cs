38e317f1e7510d7e00988ce49107cb978c5493bb
Leonid Tsarev <leo@bastilia.ru>
2019-09-01 13:35:09 +0300

    extract fields parser helper

--- Joinrpg.AspNetCore.Helpers/FormCollectionHelpers.cs
+++ Joinrpg.AspNetCore.Helpers/FormCollectionHelpers.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using JoinRpg.Helpers;
 using Microsoft.AspNetCore.Http;
 
 namespace Joinrpg.AspNetCore.Helpers
@@ -10,6 +11,14 @@ namespace Joinrpg.AspNetCore.Helpers
         public static Dictionary<string, string> ToDictionary(this IFormCollection collection)
         {
             return collection.Keys.ToDictionary(key => key, key => collection[key].First());
+        }
+
+        public static IReadOnlyDictionary<int, string> GetDynamicValuesFromPost(this HttpRequest request, string prefix)
+        {
+            var post = request.Form.ToDictionary();
+            return post.Keys.UnprefixNumbers(prefix)
+                .ToDictionary(fieldClientId => fieldClientId,
+                    fieldClientId => post[prefix + fieldClientId]);
         }
     }
 }
