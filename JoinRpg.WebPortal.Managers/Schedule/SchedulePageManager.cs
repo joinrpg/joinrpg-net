36da5531013ddd88b387bcf722fd82ae0446bc8a
Leonid Tsarev <leo@bastilia.ru>
2019-10-06 16:50:21 +0300

    Schedule minimal prototype

---
+++ JoinRpg.WebPortal.Managers/Schedule/SchedulePageManager.cs
@@ -0,0 +1,129 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using JoinRpg.Data.Interfaces;
+using JoinRpg.DataModel;
+using JoinRpg.Domain;
+using JoinRpg.Domain.Schedules;
+using JoinRpg.Interfaces;
+using JoinRpg.Web.Models.Schedules;
+using JoinRpg.WebPortal.Managers.Interfaces;
+
+namespace JoinRpg.WebPortal.Managers.Schedule
+{
+    public class SchedulePageManager
+    {
+        public SchedulePageManager(
+            IProjectRepository project,
+            ICurrentProjectAccessor currentProject,
+            ICurrentUserAccessor currentUserAccessor
+            )
+        {
+            Project = project;
+            CurrentProject = currentProject;
+            CurrentUserAccessor = currentUserAccessor;
+        }
+
+        public async Task<SchedulePageViewModel> GetSchedule()
+        {
+            var project = await Project.GetProjectWithFieldsAsync(CurrentProject.ProjectId);
+            var characters = await Project.GetCharacters(CurrentProject.ProjectId);
+            var scheduleBuilder = new ScheduleBuilder(project, characters);
+            var result = scheduleBuilder.Build();
+            var viewModel = new SchedulePageViewModel()
+            {
+                NotScheduledProgramItems = result.NotScheduled.ToViewModel(),
+                Columns = result.Rooms.ToViewModel(),
+                Rows = result.TimeSlots.ToViewModel(),
+                ConflictedProgramItems = result.Conflicted.ToViewModel(),
+                Slots = result.Slots.Select2DList(x => x.ToViewModel())
+            };
+
+            MergeSlots (viewModel);
+            return viewModel;
+        }
+
+        private void MergeSlots(SchedulePageViewModel viewModel)
+        {
+            
+
+            for (var rowIndex = 0; rowIndex < viewModel.Slots.Count; rowIndex++)
+            {
+                var slotRow = viewModel.Slots[rowIndex];
+                for (var colIndex = 0; colIndex < slotRow.Count; colIndex++)
+                {
+                    var slot = slotRow[colIndex];
+                    if (slot.IsEmpty || slot.RowSpan == 0 || slot.ColSpan == 0)
+                    {
+                        continue;
+                    }
+
+                    int CountSameSlots(IEnumerable<ProgramItemViewModel> sameSlots)
+                    {
+                        return sameSlots.TakeWhile(s => s.Id == slot.Id).Count();
+                    }
+
+                    slot.ColSpan = CountSameSlots(slotRow.Skip(colIndex));
+
+                    slot.RowSpan = CountSameSlots(viewModel.Slots.Skip(rowIndex).Select(row => row[colIndex]));
+
+                    for (var i = 0; i < slot.RowSpan; i++)
+                    {
+                        for (var j = 0; j < slot.ColSpan; j++)
+                        {
+                            if (i == 0 && j == 0)
+                            {
+                                continue;
+                            }
+                            var slotToRemove = viewModel.Slots[rowIndex + i][colIndex + j];
+                            slotToRemove.ColSpan = 0;
+                            slotToRemove.RowSpan = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        public async Task<IEnumerable<ScheduleConfigProblemsViewModel>> CheckScheduleConfiguration()
+        {
+            var project = await Project.GetProjectWithFieldsAsync(CurrentProject.ProjectId);
+
+            IEnumerable<ScheduleConfigProblemsViewModel> Impl()
+            {
+                var settings = project.Details.ScheduleSettings;
+                var roomField = new ProjectField();//settings.RoomField;
+                var timeSlotField = new ProjectField();//settings.TimeSlotField;
+                if (roomField == null || timeSlotField == null)
+                {
+                    yield return ScheduleConfigProblemsViewModel.FieldsNotSet;
+                }
+                else
+                {
+                    if (roomField.IsPublic != timeSlotField.IsPublic || roomField.CanPlayerView != timeSlotField.CanPlayerView)
+                    {
+                        yield return ScheduleConfigProblemsViewModel.InconsistentVisibility;
+                    }
+
+                    if (!project.HasMasterAccess(CurrentUserAccessor.UserId))
+                    {
+                        if (!roomField.CanPlayerView)
+                        {
+                            yield return ScheduleConfigProblemsViewModel.NoAccess;
+                        }
+                        else if (!roomField.IsPublic && !project.Claims.OfUserApproved(CurrentUserAccessor.UserId).Any())
+                        {
+                            yield return ScheduleConfigProblemsViewModel.NoAccess;
+                        }
+                    }
+                }
+            }
+
+            return Impl();
+        }
+
+        public IProjectRepository Project { get; }
+        public ICurrentProjectAccessor CurrentProject { get; }
+        public ICurrentUserAccessor CurrentUserAccessor { get; }
+    }
+}
---
+++ JoinRpg.WebPortal.Models/Schedules/TableHeaderViewModel.cs
@@ -0,0 +1,11 @@
+using JoinRpg.DataModel;
+using JoinRpg.Helpers.Web;
+
+namespace JoinRpg.Web.Models.Schedules
+{
+    public class TableHeaderViewModel
+    {
+        public string Name { get; set; }
+        public JoinHtmlString Description { get; set; }
+    }
+}
