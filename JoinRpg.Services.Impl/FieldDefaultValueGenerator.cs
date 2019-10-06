a73db01687a860106f9193b92b65652dc114130e
Leonid Tsarev <leo@bastilia.ru>
2019-10-06 22:26:01 +0300

    Write back name to bound field, if it empty and name is not fixes #966

--- JoinRpg.Services.Impl/FieldDefaultValueGenerator.cs
+++ JoinRpg.Services.Impl/FieldDefaultValueGenerator.cs
@@ -1,27 +1,33 @@
-﻿using JetBrains.Annotations;
+using JetBrains.Annotations;
 using JoinRpg.DataModel;
 using JoinRpg.Domain.CharacterFields;
 using JoinRpg.PluginHost.Interfaces;
 
 namespace JoinRpg.Services.Impl
 {
-  [UsedImplicitly]
-  internal class FieldDefaultValueGenerator : IFieldDefaultValueGenerator
-  {
-    public FieldDefaultValueGenerator(IPluginFactory pluginFactory)
+    [UsedImplicitly]
+    internal class FieldDefaultValueGenerator : IFieldDefaultValueGenerator
     {
-      PluginFactory = pluginFactory;
-    }
+        public FieldDefaultValueGenerator(IPluginFactory pluginFactory)
+        {
+            PluginFactory = pluginFactory;
+        }
 
-    private IPluginFactory PluginFactory { get; }
-    public string CreateDefaultValue(Claim claim, ProjectField feld) => null;
+        private IPluginFactory PluginFactory { get; }
+        public string CreateDefaultValue(Claim claim, ProjectField feld) => null;
 
-    public string CreateDefaultValue(Character character, ProjectField field)
-    {
-      return character != null
-        ? PluginFactory
-          .GenerateDefaultCharacterFieldValue(character, field)
-        : null;
+        public string CreateDefaultValue(Character character, ProjectField field)
+        {
+            if (field == field.Project.Details.CharacterNameField && character != null)
+            {
+                return character.CharacterName;
+                // It helps battle akward situations where names was re-bound to some new field
+                // and empty values start overwriting names
+            }
+            return character != null
+              ? PluginFactory
+                .GenerateDefaultCharacterFieldValue(character, field)
+              : null;
+        }
     }
-  }
 }
