bd65254f5de44a21de8966c26269bfdd69835b17
Vladislav Kiselev <shiko-1st@yandex.ru>
2019-10-07 16:51:37 +0300

    Fix encoding used in pscb messages

--- PscbApi/Helpers/StringExtensions.cs
+++ PscbApi/Helpers/StringExtensions.cs
@@ -1,4 +1,7 @@
 // ReSharper disable CheckNamespace
+
+using System.Text;
+
 namespace PscbApi
 {
     internal static class StringExtensions
@@ -7,5 +10,13 @@ namespace PscbApi
             => string.IsNullOrWhiteSpace(source) || source.Length < maxLength
                 ? source
                 : source.Substring(0, maxLength);
+
+        public static byte[] ToUtf8Bytes(this string source)
+            => Encoding.Convert(
+                Encoding.Unicode,
+                Encoding.UTF8,
+                Encoding.Unicode.GetBytes(source));
+
+
     }
 }
