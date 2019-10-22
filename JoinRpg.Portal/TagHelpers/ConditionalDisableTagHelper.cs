6e52284a510165ad651e14c77f36661cbe4d2933
Leonid Tsarev <leo@leotsarev.ru>
2019-10-22 17:16:40 +0300

    repair menu (again)

---
+++ JoinRpg.Portal/TagHelpers/ConditionalDisableTagHelper.cs
@@ -0,0 +1,24 @@
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace JoinRpg.Portal.TagHelpers
+{
+    // TODO test and enable [HtmlTargetElement("button")]
+    [HtmlTargetElement("a")]
+    [HtmlTargetElement("li")] //for bootstrap menus
+    public class ConditionalDisableTagHelper : TagHelper
+    {
+        [HtmlAttributeName("asp-is-disabled")]
+        public bool IsDisabled { set; get; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (IsDisabled)
+            {
+                output.AddClass("disabled", HtmlEncoder.Default);
+            }
+            base.Process(context, output);
+        }
+    }
+}
