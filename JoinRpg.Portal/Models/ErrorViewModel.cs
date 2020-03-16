afc8be5679fb24ba986dc347517aad162279d54c
Sergey Fedorov <sergey.fedorov@mercer.com>
2020-03-16 18:57:27 +0300

    htmlHelper updates

---
+++ JoinRpg.Portal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoinRpg.Portal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
