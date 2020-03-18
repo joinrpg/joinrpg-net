0d94c563da8c0e7ad3d96bc6c0e67fb2560172ce
Sergey Fedorov <sergey.fedorov@mercer.com>
2020-03-18 12:47:27 +0300

    view fixes

--- JoinRpg.Portal/AppCodeCompiled/JoinDropdownMvcHelper.cs
+++ JoinRpg.Portal/AppCodeCompiled/JoinDropdownMvcHelper.cs
@@ -6,40 +6,33 @@ using JoinRpg.Web.Models.CommonTypes;
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace JoinRpg.Portal
+namespace JoinRpg.Portal.AppCodeCompiled
 {
     public static class JoinDropdownMvcHelper
     {
-        public static IHtmlContent JoinFormDropdownFor<TModel, TValue>(
+        public static HtmlString JoinFormDropdownFor<TModel, TValue>(
             this IHtmlHelper<TModel> self,
             Expression<Func<TModel, TValue>> expression,
             IEnumerable<JoinSelectListItem> items)
         {
-            return new HtmlString(
-                string.Concat(
-                    new HtmlString("<div class=\"form-group\">"),
-                    self.LabelFor(expression, htmlAttributes: new { @class = "control-label col-md-2" }),
-                    new HtmlString("<div class=\"col-md-10\">"),
-                    DropDownListFor(self, expression, items),
-                    self.DescriptionFor(expression),
-                    self.ValidationMessageFor(expression, "", new { @class = "text-danger" }),
-                    new HtmlString("</div>"),
-                    new HtmlString("</div>")
-                ));
-        }
-
-        public static IHtmlContent DropDownListFor<TModel, TValue>(
-            this IHtmlHelper<TModel> self,
-            Expression<Func<TModel, TValue>> expression,
-            IEnumerable<JoinSelectListItem> select)
-            => self.DropDownListFor(expression, select.ToSelectListItems(), new { @class = "form-control" } );
-
-        public static IEnumerable<SelectListItem> ToSelectListItems(this IEnumerable<JoinSelectListItem> items) =>
-            items.Select(item => new SelectListItem()
+            var select = items.Select(item => new SelectListItem()
             {
                 Text = item.Text,
                 Value = item.Value.ToString(),
                 Selected = item.Selected,
             });
+
+            return new HtmlString(
+                string.Concat(
+                    new HtmlString("<div class=\"form-group\">"),
+                    self.LabelFor(expression, htmlAttributes: new {@class = "control-label col-md-2"}),
+                    new HtmlString("<div class=\"col-md-10\">"),
+                    self.DropDownListFor(expression, select, htmlAttributes: new {@class = "form-control"}),
+                    self.DescriptionFor(expression),
+                    self.ValidationMessageFor(expression, "", new {@class = "text-danger"}),
+                    new HtmlString("</div>"),
+                    new HtmlString("</div>")
+                ));
+        }
     }
 }
