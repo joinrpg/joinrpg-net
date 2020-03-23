4afbd8150abbd03deb55576027502e3672186327
Leonid Tsarev <leo@leotsarev.ru>
2020-03-23 11:05:49 +0300

    Remove System.Web from Joinrpg.Helpers.Web

--- JoinRpg.Helpers.Web/JoinHtmlString.cs
+++ JoinRpg.Helpers.Web/JoinHtmlString.cs
@@ -1,19 +1,15 @@
 using System.IO;
 using System.Text.Encodings.Web;
-using System.Web;
 using Microsoft.AspNetCore.Html;
-using CoreHtmlString = Microsoft.AspNetCore.Html.HtmlString;
-using WebHtmlString = System.Web.HtmlString;
 
 namespace JoinRpg.Helpers.Web
 {
-    /// <inheritdoc cref="System.Web.IHtmlString" />
-    /// <inheritdoc cref="Microsoft.AspNetCore.Html.IHtmlContent" />
+    /// <inheritdoc cref="IHtmlContent" />
     /// <summary>
     /// Proxy class that allows conversion from System.Web style strings to Microsoft.AspNetCore versions
     /// Will be removed after porting to ASP.NET Core
     /// </summary>
-    public sealed class JoinHtmlString : IHtmlString, IHtmlContent
+    public sealed class JoinHtmlString : IHtmlContent
     {
         private string Value { get; }
 
@@ -25,21 +21,12 @@ namespace JoinRpg.Helpers.Web
         /// <summary>
         /// Converts from AspNetCore style string
         /// </summary>
-        public static implicit operator JoinHtmlString(CoreHtmlString content)
+        public static implicit operator JoinHtmlString(HtmlString content)
         {
             var stringWriter = new StringWriter();
             content.WriteTo(stringWriter, HtmlEncoder.Default);
             var value = stringWriter.ToString();
             return new JoinHtmlString(value);
-        }
-
-        /// <summary>
-        /// Converts from System.Web style string
-        /// </summary>
-        /// <param name="content"></param>
-        public static implicit operator JoinHtmlString(WebHtmlString content)
-        {
-            return new JoinHtmlString(content.ToHtmlString());
         }
 
         /// <summary>
