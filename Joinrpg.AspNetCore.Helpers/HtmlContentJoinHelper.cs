9de0593d9243b2515a37e9b8a2af0e8212e516fb
Leonid Tsarev <leo@leotsarev.ru>
2020-04-05 12:34:43 +0300

    Add HtmlContentJoinHelper to accomodate to IHtmlContent semantic changes in core (.ToString() no longer returns rendered content)

---
+++ Joinrpg.AspNetCore.Helpers/HtmlContentJoinHelper.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Joinrpg.AspNetCore.Helpers
+{
+    /// <summary>
+    /// Helper to accomodate to IHtmlContent semantic changes in core (.ToString() no longer returns rendered content)
+    /// </summary>
+    public static class HtmlContentJoinHelper
+    {
+        /// <summary>
+        /// Join IHtmlContents them with separator
+        /// </summary>
+        public static IHtmlContent JoinList(this IHtmlHelper self, string rawSeparator, IEnumerable<IHtmlContent> contents)
+        {
+            var builder = new HtmlContentBuilder();
+            bool writeSep = false;
+            foreach (var c in contents)
+            {
+                if (writeSep)
+                {
+                    builder.AppendHtml(rawSeparator);
+                }
+                writeSep = true;
+                builder.AppendHtml(c);
+            }
+            return builder;
+        }
+
+        /// <summary>
+        /// Call Html.DisplayFor(..) for every element in list and then joins them with separator
+        /// </summary>
+        public static IHtmlContent JoinDisplayFor<TModel, TListItem, TResult>(this IHtmlHelper<TModel> self, string rawSeparator, IEnumerable<TListItem> list,  Func<TListItem, TResult> selector)
+        {
+            IEnumerable<IHtmlContent> ToDisplayFor()
+            {
+                foreach (var item in list)
+                {
+                    var selectedItem = selector(item);
+                    yield return self.DisplayFor(model => item);
+                }    
+            }
+
+            return self.JoinList(rawSeparator, ToDisplayFor());
+        }
+
+        /// <summary>
+        /// Call Html.DisplayFor(..) for every element in list and then joins them with separator
+        /// </summary>
+        public static IHtmlContent JoinDisplayFor<TModel, TListItem>(this IHtmlHelper<TModel> self, string rawSeparator, IEnumerable<TListItem> list)
+        {
+            return self.JoinDisplayFor(rawSeparator, list, item => item);
+        }
+    }
+}
