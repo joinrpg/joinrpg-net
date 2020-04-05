37ed3ea903c1edb057ef392efab3dfb1d64f841c
Leonid Tsarev <leo@leotsarev.ru>
2020-04-05 12:34:58 +0300

    repair masters page

---
+++ JoinRpg.WebPortal.Models/PermissionBadgeViewModel.cs
@@ -0,0 +1,9 @@
+namespace JoinRpg.Web.Models
+{
+    public class PermissionBadgeViewModel
+    {
+        public bool Value { get; set; }
+        public string Description { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
