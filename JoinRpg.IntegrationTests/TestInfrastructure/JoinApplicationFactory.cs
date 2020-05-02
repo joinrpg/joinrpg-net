a6d992c8f5e19d528ee7ace115d5e66a01ef2d2c
Leonid Tsarev <leo@leotsarev.ru>
2020-05-02 17:09:23 +0300

    stub recaptcha

--- JoinRpg.IntegrationTests/TestInfrastructure/JoinApplicationFactory.cs
+++ JoinRpg.IntegrationTests/TestInfrastructure/JoinApplicationFactory.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Reflection;
+using Autofac;
 using JoinRpg.Portal;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 
 namespace JoinRpg.IntegrationTests.TestInfrastructure
 {
@@ -9,7 +13,22 @@ namespace JoinRpg.IntegrationTests.TestInfrastructure
         protected override void ConfigureWebHost(IWebHostBuilder builder)
         {
             base.ConfigureWebHost(builder);
-            builder.UseEnvironment("IntegrationTest");
+            builder
+                .UseEnvironment("IntegrationTest")
+                .ConfigureTestContainer((System.Action<ContainerBuilder>)(containerBuilder =>
+                {
+                    containerBuilder
+                        .RegisterAssemblyTypes(Assembly.GetExecutingAssembly())
+                        .Where(IsStub)
+                        .AsSelf()
+                        .AsImplementedInterfaces();
+
+                }));
+
+            bool IsStub(Type type)
+            {
+                return type.FullName?.StartsWith("JoinRpg.IntegrationTests.TestInfrastructure.Stubs") == true;
+            }
         }
     }
 }
