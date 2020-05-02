2bf2cfc07be4ab0073579c0a0296ce8294438d36
Leonid Tsarev <leo@leotsarev.ru>
2020-05-02 12:48:32 +0300

    remove inverse property annotation

--- JoinRpg.DataModel/Character.cs
+++ JoinRpg.DataModel/Character.cs
@@ -54,11 +54,11 @@ namespace JoinRpg.DataModel
 
     public virtual IEnumerable<Claim> Claims => Project.Claims.Where(c => c.CharacterId == CharacterId);
 
-    [CanBeNull, InverseProperty(null)]
+    [CanBeNull/*, InverseProperty(null)*/]
     public virtual  Claim ApprovedClaim { get; set; }
 
-    [ForeignKey(nameof(ApprovedClaim)), InverseProperty(null)]
-    public int? ApprovedClaimId { get; set; }
+    [ForeignKey(nameof(ApprovedClaim))/*, InverseProperty(null)*/]
+        public int? ApprovedClaimId { get; set; }
 
     public virtual ICollection<UserSubscription> Subscriptions { get; set; }
     public bool IsRoot => false; //Character is not "root group"
