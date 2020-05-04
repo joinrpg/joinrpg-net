615bb84a7adfd76684467675546a20fb08757108
Leonid Tsarev <leo@leotsarev.ru>
2020-05-04 21:25:22 +0300

    Annotate Helpers

--- JoinRpg.Helpers/DictionaryStaticExtensions.cs
+++ JoinRpg.Helpers/DictionaryStaticExtensions.cs
@@ -6,16 +6,17 @@ namespace JoinRpg.Helpers
 {
     public static class DictionaryStaticExtensions
     {
-        public static TValue GetValueOrDefault<TKey, TValue>([NotNull]
+        public static TValue? GetValueOrDefault<TKey, TValue>([NotNull]
             this IReadOnlyDictionary<TKey, TValue> data,
             TKey key)
+            where TValue: class
         {
             if (data == null)
             {
                 throw new ArgumentNullException(nameof(data));
             }
 
-            return data.ContainsKey(key) ? data[key] : default(TValue);
+            return data.ContainsKey(key) ? data[key] : default;
         }
     }
 }
