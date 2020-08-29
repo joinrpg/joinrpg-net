049258e1071898c55aed6edbaf9062d7df3f5006
Leonid Tsarev <leo@leotsarev.ru>
2020-08-29 19:40:09 +0300

    add integration test to ensure russian datetime accepted by binders

---
+++ src/JoinRpg.IntegrationTests/Scenarios/CheckDateTimeMappingTest.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JoinRpg.IntegrationTest.TestInfrastructure.MockControllers;
+using JoinRpg.IntegrationTests.TestInfrastructure;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Shouldly;
+using Xunit;
+
+namespace JoinRpg.IntegrationTest.Scenarios
+{
+    public class CheckDateTimeMappingTest : IClassFixture<JoinApplicationFactoryWithMockControllers>
+    {
+        private readonly JoinApplicationFactory factory;
+
+        public CheckDateTimeMappingTest(JoinApplicationFactoryWithMockControllers factory) => this.factory = factory;
+
+        [Fact]
+        public async Task CheckDateTime()
+        {
+            var client = factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+            var content = new FormUrlEncodedContent(
+                    new[]
+                    {
+                        new KeyValuePair<string, string>("Date", "29.8.2020"),
+                    });
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "test/mockdatetime");
+            request.Content = content;
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            MockDateTimeController.LastCalledDateTime.ShouldBe(new DateTime(2020, 8, 29));
+        }
+
+        [Fact]
+        public async Task CheckDateTimeWithEngLanguageRequested()
+        {
+            var client = factory.CreateClient(new WebApplicationFactoryClientOptions { AllowAutoRedirect = false });
+            var content = new FormUrlEncodedContent(
+                    new[]
+                    {
+                        new KeyValuePair<string, string>("Date", "29.8.2020"),
+                    });
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "test/mockdatetime");
+            request.Headers.AcceptLanguage.ParseAdd("en-US");
+            request.Content = content;
+
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+            MockDateTimeController.LastCalledDateTime.ShouldBe(new DateTime(2020, 8, 29));
+        }
+    }
+}
---
+++ src/JoinRpg.IntegrationTests/TestInfrastructure/MockControllers/MockDateTimeController.cs
@@ -0,0 +1,23 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JoinRpg.IntegrationTest.TestInfrastructure.MockControllers
+{
+    [AllowAnonymous]
+    public class MockDateTimeController : Controller
+    {
+        public static DateTime LastCalledDateTime;
+
+        [HttpPost("test/mockdatetime")]
+        public void Test(TestDateTimeModel model)
+        {
+            LastCalledDateTime = model.Date;
+        }
+    }
+
+    public class TestDateTimeModel
+    {
+        public DateTime Date { get; set; }
+    }
+}
