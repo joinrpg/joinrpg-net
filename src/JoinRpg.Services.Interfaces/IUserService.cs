b7984515e89c76142c825254b468f1d202c4cc5d
Leonid Tsarev <leo@leotsarev.ru>
2020-08-30 14:52:50 +0300

    Password change now use UserManager instread of changing in DB directly (#1141)

--- src/JoinRpg.Services.Interfaces/IUserService.cs
+++ src/JoinRpg.Services.Interfaces/IUserService.cs
@@ -6,7 +6,6 @@ namespace JoinRpg.Services.Interfaces
     public interface IUserService
     {
         Task UpdateProfile(int userId, string surName, string fatherName, string bornName, string prefferedName, Gender gender, string phoneNumber, string nicknames, string groupNames, string skype, string vk, string livejournal, string telegram);
-        Task ChangeEmail(int userId, string newEmail);
         Task SetAdminFlag(int userId, bool administratorFlag);
         Task SetVerificationFlag(int userId, bool verificationFlag);
     }
