a081e70cb1c50a0119c6144e0b172c6942eed910
Andrey Krivtsov <Andrey.Krivtsov@softline.com>
2020-12-06 19:10:37 +0300

    test Helm

--- Deployment/Portal/Prepare-ChartValues.ps1
+++ Deployment/Portal/Prepare-ChartValues.ps1
@@ -19,6 +19,8 @@ $data = @{}
 
 $secrets = Get-AzKeyVaultSecret -VaultName $VaultName -WarningAction SilentlyContinue
 
+Write-Host "Read secrets from KeyVault: $VaultName, prefix: $Prefix"
+
 $secrets | ?{ $_.Name.StartsWith($Prefix) } |
 %{
     $name = $_.Name
@@ -29,11 +31,16 @@ $secrets | ?{ $_.Name.StartsWith($Prefix) } |
     $name = $name.Substring($Prefix.Length).Replace("--","__");
 
     $data.Add($name, $value);
+
+    Write-Host "  Found Secret: $name"
 }
 
 "secrets:" | Out-File -FilePath $Path -Encoding utf8 -Force -Append
 
 $data.Keys | %{ "  $($_): `"$($data[$_])`"" } | Out-File -FilePath $Path -Encoding utf8 -Append
+
+Write-Host "Set ingress hostname: $IngressHost"
+Write-Host "Set ingress path: $IngressPath"
 
 "ingress:" | Out-File -FilePath $Path -Encoding utf8 -Append
 "  host: $IngressHost" | Out-File -FilePath $Path -Encoding utf8 -Append
