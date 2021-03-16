c45eb7422b1b2b9b257d5082cf074c07fecc0c39
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-03-17 00:55:03 +0100

    Apply dotnet format changes

--- src/JoinRpg.Dal.Impl/Migrations/202103161652154_RemoveProjectAclTokenGeneration.cs
+++ src/JoinRpg.Dal.Impl/Migrations/202103161652154_RemoveProjectAclTokenGeneration.cs
@@ -1,15 +1,15 @@
-﻿namespace JoinRpg.Dal.Impl.Migrations
+namespace JoinRpg.Dal.Impl.Migrations
 {
     using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class RemoveProjectAclTokenGeneration : DbMigration
     {
         public override void Up()
         {
             AlterColumn("dbo.ProjectAcls", "Token", c => c.Guid(nullable: false));
         }
-        
+
         public override void Down()
         {
             AlterColumn("dbo.ProjectAcls", "Token", c => c.Guid(nullable: false));
