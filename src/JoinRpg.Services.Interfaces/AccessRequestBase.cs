2d468e12e4f9eccd556df03cb43955495d1e8d1a
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-04-01 00:34:13 +0200

    Add some xml docs

--- src/JoinRpg.Services.Interfaces/AccessRequestBase.cs
+++ src/JoinRpg.Services.Interfaces/AccessRequestBase.cs
@@ -4,13 +4,37 @@ namespace JoinRpg.Services.Interfaces
     {
         public int ProjectId { get; set; }
         public int UserId { get; set; }
+        /// <summary>
+        /// Ability to grand and edit roles for other members, and give master rights
+        /// </summary>
         public bool CanGrantRights { get; set; }
+        /// <summary>
+        /// Ability to edit project claim's fields and character's fields
+        /// </summary>
         public bool CanChangeFields { get; set; }
+        /// <summary>
+        /// Ability to edit project and it's properties like name, status and etc.
+        /// </summary>
         public bool CanChangeProjectProperties { get; set; }
+        /// <summary>
+        /// Ability to change status of active project's claims (accept, reject, move)
+        /// </summary>
         public bool CanManageClaims { get; set; }
+        /// <summary>
+        /// Ability to edit current project roles (create new role groups, add/remove characters to it)
+        /// </summary>
         public bool CanEditRoles { get; set; }
+        /// <summary>
+        /// Ability to manage project finances
+        /// </summary>
         public bool CanManageMoney { get; set; }
+        /// <summary>
+        /// Ability to send massive mails to project members
+        /// </summary>
         public bool CanSendMassMails { get; set; }
+        /// <summary>
+        /// Ability to edit project plots, sets role groups to it, visibility and etc.
+        /// </summary>
         public bool CanManagePlots { get; set; }
         public bool CanManageAccommodation { get; set; }
         public bool CanSetPlayersAccommodations { get; set; }
