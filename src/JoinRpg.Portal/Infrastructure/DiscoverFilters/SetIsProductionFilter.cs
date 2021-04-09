c6026f7a9cc25ce9f2677891e185d1ef007731ed
Stanislav Khavruk <stas_khavruk@outlook.com>
2021-04-10 01:21:45 +0200

    Make valid domain check

--- src/JoinRpg.Portal/Infrastructure/DiscoverFilters/SetIsProductionFilter.cs
+++ src/JoinRpg.Portal/Infrastructure/DiscoverFilters/SetIsProductionFilter.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -14,8 +15,13 @@ namespace JoinRpg.Portal.Infrastructure
         {
             if (context.Result is ViewResult viewResult)
             {
+                // Request.Host contains URL with port "beta.example.com:5001"
+                // Request.Host.Host contains URL without port "beta.example.com"
                 var hostHost = context.HttpContext.Request.Host.Host;
-                viewResult.ViewData["IsProduction"] = hostHost.Contains("joinrpg.ru");
+                // For check, we should take top domain word
+                var topDomain = hostHost.Split('.').First();
+                // And make it Dev if starts with "www" or subdomain (i.e. "dev")
+                viewResult.ViewData["IsProduction"] = topDomain == "joinrpg";
                 viewResult.ViewData["FullHostName"] = context.HttpContext.Request.Scheme + hostHost;
             }
             return base.OnResultExecutionAsync(context, next);
