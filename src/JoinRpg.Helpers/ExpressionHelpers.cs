dc12a5c812f69bb754bd5afe14c64e7262d9fb06
Leonid Tsarev <leo@leotsarev.ru>
2021-05-03 17:00:42 +0300

    Improve expression helper

--- src/JoinRpg.Helpers/ExpressionHelpers.cs
+++ src/JoinRpg.Helpers/ExpressionHelpers.cs
@@ -8,28 +8,35 @@ namespace JoinRpg.Helpers
     public static class ExpressionHelpers
     {
         [CanBeNull]
-        public static string? AsPropertyName<T1, T2>(this Expression<Func<T1, T2>> expression) => expression.AsPropertyAccess()?.Name;
+        public static string? AsPropertyName<T1, T2>(this Expression<Func<T1, T2>> expression)
+            => expression.AsPropertyAccess()?.Name;
+
+        public static string? AsPropertyName<T1>(this Expression<Func<T1>> expression)
+            => expression.AsPropertyAccess()?.Name;
 
         [CanBeNull]
-        public static PropertyInfo? AsPropertyAccess<T1, T2>(
-            this Expression<Func<T1, T2>> expression)
+        public static PropertyInfo? AsPropertyAccess<T1, T2>(this Expression<Func<T1, T2>> expression)
+            => AsPropetyAccessImpl(expression.Body);
+
+        [CanBeNull]
+        public static PropertyInfo? AsPropertyAccess<T>(this Expression<Func<T>> expression)
+         => AsPropetyAccessImpl(expression.Body);
+
+        private static PropertyInfo? AsPropetyAccessImpl(Expression expression)
         {
-            if (expression.Body is UnaryExpression convertExpression &&
+            if (expression is UnaryExpression convertExpression &&
                 convertExpression.NodeType == ExpressionType.Convert)
             {
                 return AsPropertyAccess(convertExpression.Operand);
             }
 
-            return AsPropertyAccess(expression.Body);
+            return AsPropertyAccess(expression);
         }
 
         [CanBeNull]
         private static PropertyInfo? AsPropertyAccess(Expression body)
         {
-            var memberExpression = body as MemberExpression;
-            return memberExpression?.Member as PropertyInfo;
+            return (body as MemberExpression)?.Member as PropertyInfo;
         }
-
-        public static string AsPropertyName<T1>(this Expression<Func<T1>> expression) => ((MemberExpression)expression.Body).Member.Name;
     }
 }
