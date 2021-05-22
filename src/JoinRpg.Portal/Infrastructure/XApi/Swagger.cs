4ad59b1043cfc79483b49b635710e88ad83acc63
Leonid Tsarev <leo@leotsarev.ru>
2021-05-22 10:18:07 +0300

    Optimize swagger

--- src/JoinRpg.Portal/Infrastructure/XApi/Swagger.cs
+++ src/JoinRpg.Portal/Infrastructure/XApi/Swagger.cs
@@ -17,8 +17,6 @@ namespace JoinRpg.Portal.Infrastructure
 
     internal class Swagger
     {
-        private const string ForwardedPrefixHeader = "X-Forwarded-Prefix";
-
         internal static void ConfigureSwagger(SwaggerGenOptions c)
         {
             var securityScheme = new OpenApiSecurityScheme
@@ -52,31 +50,18 @@ namespace JoinRpg.Portal.Infrastructure
             c.DocumentFilter<SwaggerXGameApiFilter>();
         }
 
-        internal static void Configure(SwaggerOptions options) => options.PreSerializeFilters.Add(ReverseProxyPreSerializeFilter);
+        internal static void Configure(SwaggerOptions options) { }
 
-        internal static void ConfigureUI(SwaggerUIOptions c) => c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+        internal static void ConfigureUI(SwaggerUIOptions c)
+        {
+            c.SwaggerEndpoint("v1/swagger.json", "My API V1");
+            c.ConfigObject.DeepLinking = true;
+        }
 
         internal static Task RedirectToSwagger(HttpContext ctx)
         {
             ctx.Response.Redirect("swagger/");
             return Task.CompletedTask;
-        }
-
-        private static void ReverseProxyPreSerializeFilter(OpenApiDocument document, HttpRequest request)
-        {
-            string prefix;
-            if (!request.Headers.TryGetValue(ForwardedPrefixHeader, out var prefixHeaderValues) ||
-                prefixHeaderValues.Count == 0 ||
-                string.IsNullOrEmpty(prefix = prefixHeaderValues[0]))
-            {
-                return;
-            }
-
-            document.Servers.Add(new OpenApiServer
-            {
-                Url = prefix,
-                Description = "Reverse proxy server",
-            });
         }
 
         private class SwaggerXGameApiFilter : IDocumentFilter
