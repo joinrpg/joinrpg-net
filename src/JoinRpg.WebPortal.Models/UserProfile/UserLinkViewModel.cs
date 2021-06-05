880bbf697f33ce8bb6379fe2eea8d847b44fd686
Leonid Tsarev <leo@leotsarev.ru>
2021-06-05 14:58:04 +0300

    Add ILinkable to UserLinkViewModel

--- src/JoinRpg.WebPortal.Models/UserProfile/UserLinkViewModel.cs
+++ src/JoinRpg.WebPortal.Models/UserProfile/UserLinkViewModel.cs
@@ -1,18 +1,29 @@
+using System;
 using JoinRpg.DataModel;
 using JoinRpg.Domain;
+using JoinRpg.Services.Interfaces;
 
 namespace JoinRpg.Web.Models.UserProfile
 {
     public record UserLinkViewModel(
         int UserId,
         string DisplayName)
+        : ILinkable
     {
         public UserLinkViewModel(User user) : this(user.UserId, user.GetDisplayName().Trim())
         {
 
         }
 
-        public static UserLinkViewModel? FromOptional(User user)
+        public Uri GetUri(IUriService uriService) => uriService.GetUri(this);
+
+        LinkType ILinkable.LinkType => LinkType.ResultUser;
+
+        string ILinkable.Identification => UserId.ToString();
+
+        int? ILinkable.ProjectId => null;
+
+        public static UserLinkViewModel? FromOptional(User? user)
             => user is null ? null : new UserLinkViewModel(user);
     }
 }
