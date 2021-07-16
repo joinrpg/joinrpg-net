bc479a7dd63f83df6a88360d037c066f070d789d
Leonid Tsarev <leo@leotsarev.ru>
2021-07-16 22:36:24 +0300

    AddCharacterRequest to record

--- src/JoinRpg.Services.Interfaces/AddCharacterRequest.cs
+++ src/JoinRpg.Services.Interfaces/AddCharacterRequest.cs
@@ -2,15 +2,14 @@ using System.Collections.Generic;
 
 namespace JoinRpg.Services.Interfaces
 {
-    public class AddCharacterRequest
-    {
-        public int ProjectId { get; set; }
-        public string Name { get; set; }
-        public bool IsPublic { get; set; }
-        public IReadOnlyCollection<int> ParentCharacterGroupIds { get; set; }
-        public bool IsAcceptingClaims { get; set; }
-        public bool HidePlayerForCharacter { get; set; }
-        public bool IsHot { get; set; }
-        public IReadOnlyDictionary<int, string?> FieldValues { get; set; }
-    }
+    public record AddCharacterRequest(
+        int ProjectId,
+        string Name,
+        bool IsPublic,
+        IReadOnlyCollection<int> ParentCharacterGroupIds,
+        bool IsAcceptingClaims,
+        bool HidePlayerForCharacter,
+        bool IsHot,
+        IReadOnlyDictionary<int, string?> FieldValues)
+    { }
 }
