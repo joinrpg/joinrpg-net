ed9fce8fdee3efc8a1208e7eee466a22eeb0f04d
Leonid Tsarev <leo@leotsarev.ru>
2021-07-17 18:55:09 +0300

    fix formatting

--- src/JoinRpg.WebPortal.Models/Claims/FinanceOperationActionView.cs
+++ src/JoinRpg.WebPortal.Models/Claims/FinanceOperationActionView.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using JetBrains.Annotations;
 
 namespace JoinRpg.Web.Models
--- src/JoinRpg.WebPortal.Models/Comments/CommentFinanceOperationViewModel.cs
+++ src/JoinRpg.WebPortal.Models/Comments/CommentFinanceOperationViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using JoinRpg.DataModel;
 
 namespace JoinRpg.Web.Models
