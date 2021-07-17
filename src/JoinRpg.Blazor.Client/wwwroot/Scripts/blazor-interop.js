c345d6d41d8f073d33526381521f4d3d867e33c2
Leonid Tsarev <leo@leotsarev.ru>
2021-07-17 22:29:48 +0300

    Add IntSelector component

--- src/JoinRpg.Blazor.Client/wwwroot/Scripts/blazor-interop.js
+++ src/JoinRpg.Blazor.Client/wwwroot/Scripts/blazor-interop.js
@@ -1,5 +1,11 @@
 ﻿window.joinmethods = {
   GetDocumentCookie: function () {
     return { content: document.cookie };
+  },
+  InitBootstrapSelect: function (ref) {
+    $(ref).selectpicker();
+  },
+  RefreshBootstrapSelect: function (ref) {
+    $(ref).selectpicker('refresh');
   }
 }
