ffa11b5cd6941b18835e125e0d0f3a4331f539a0
Leonid Tsarev <leo@leotsarev.ru>
2021-10-14 19:11:00 +0300

    Fix problem with editing a slot (#1547)

--- src/JoinRpg.Dal.Impl/Repositories/CharacterRepositoryImpl.cs
+++ src/JoinRpg.Dal.Impl/Repositories/CharacterRepositoryImpl.cs
@@ -88,7 +88,7 @@ namespace JoinRpg.Dal.Impl.Repositories
                 UpdatedAt = character.UpdatedAt,
                 IsActive = character.IsActive,
                 InGame = character.InGame,
-                CharacterTypeInfo = new(character.IsAvailable ? CharacterType.Player : CharacterType.NonPlayer, character.IsHot, character.CharacterSlotLimit),
+                CharacterTypeInfo = new(character.CharacterType, character.IsHot, character.CharacterSlotLimit),
                 JsonData = character.JsonData,
                 ApprovedClaim = await Ctx.Set<Claim>()
                     .Where(claim => claim.CharacterId == characterId &&
