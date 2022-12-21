0421718570f9d8101f49ebbc1cfc21fcf78b68ec
Leonid Tsarev <leo@leotsarev.ru>
2022-12-21 18:46:43 +0300

    Refactor blazor selectors

---
+++ src/JoinRpg.Web.ProjectMasterTools/ResponsibleMaster/AddRespMasterRuleViewModel.cs
@@ -0,0 +1,9 @@
+using JoinRpg.Web.ProjectCommon;
+
+namespace JoinRpg.Web.ProjectMasterTools.ResponsibleMaster;
+public class AddRespMasterRuleViewModel
+{
+    public CharacterGroupDto Group { get; set; } = null!;
+
+    public MasterViewModel Master { get; set; } = null!;
+}
