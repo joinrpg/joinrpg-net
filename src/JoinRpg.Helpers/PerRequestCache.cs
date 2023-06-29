285826c6fb1d7e352d7bc8216f9a0e8a77962cae
Leonid Tsarev <leo@leotsarev.ru>
2023-06-29 16:53:12 +0300

    Add cache decorator

---
+++ src/JoinRpg.Helpers/PerRequestCache.cs
@@ -0,0 +1,28 @@
+using System.Collections.Concurrent;
+
+namespace JoinRpg.Helpers;
+
+public class PerRequestCache<TKey, TValue>
+    where TKey : notnull, IEquatable<TKey>
+    where TValue : class
+{
+    private readonly ConcurrentDictionary<TKey, TValue> cache = new();
+    public TValue? TryGet(TKey key)
+    {
+        if (cache.TryGetValue(key, out var value))
+        {
+            return value;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void Set(TKey key, TValue value)
+    {
+        _ = cache.TryAdd(key, value);
+    }
+
+
+}
