b6b5494e7bb6b40dc0b4fb0f65c80da23165f768
Leonid Tsarev <leo@leotsarev.ru>
2023-09-14 14:51:47 +0300

    Улучшить поиск пользователей по ВК

--- src/JoinRpg.Services.Impl/Search/Providers/UserSearchProvider.cs
+++ src/JoinRpg.Services.Impl/Search/Providers/UserSearchProvider.cs
@@ -2,8 +2,10 @@ using System.Data.Entity;
 using JoinRpg.Data.Write.Interfaces;
 using JoinRpg.DataModel;
 using JoinRpg.Domain;
+using JoinRpg.Helpers;
 using JoinRpg.PrimitiveTypes;
 using JoinRpg.Services.Interfaces.Search;
+using LinqKit;
 
 namespace JoinRpg.Services.Impl.Search;
 
@@ -26,28 +28,49 @@ internal class UserSearchProvider : ISearchProvider
 
     public async Task<IReadOnlyCollection<ISearchResult>> SearchAsync(int? currentUserId, string searchString)
     {
+        searchString = searchString.Trim();
+
+        if (searchString == "")
+        {
+            return Array.Empty<ISearchResult>();
+        }
+
         (var idToFind, var matchByIdIsPerfect) = SearchKeywordsResolver.TryGetId(searchString, keysForPerfectMath);
 
-        var results =
-          await
-            unitOfWork.GetDbSet<User>()
-              .Where(user =>
-                //TODO Convert to PredicateBuilder
-                user.UserId == idToFind
-                || user.Email.Contains(searchString)
-                || user.FatherName!.Contains(searchString)
-                || user.BornName!.Contains(searchString)
-                || user.SurName!.Contains(searchString)
-                || user.PrefferedName!.Contains(searchString)
-                || (user.Extra != null && user.Extra.Nicknames != null && user.Extra.Nicknames.Contains(searchString))
-                || (idToFind != null && user.Extra != null && user.Extra.Vk! == "id" + idToFind)
-                || (idToFind != null && user.ExternalLogins.Any(el => el.Key == idToFind.ToString()))
-              )
-              .ToListAsync();
+        var predicateBuilder = PredicateBuilder.New<User>();
+
+        predicateBuilder = predicateBuilder.Or(
+                user => user.Email.Contains(searchString)
+                    || user.FatherName!.Contains(searchString)
+                    || user.BornName!.Contains(searchString)
+                    || user.SurName!.Contains(searchString)
+                    || user.PrefferedName!.Contains(searchString)
+                    || (user.Extra != null && user.Extra.Nicknames != null && user.Extra.Nicknames.Contains(searchString)));
+
+        if (idToFind is int intId)
+        {
+            predicateBuilder = predicateBuilder.Or(user => user.UserId == intId);
+            predicateBuilder = predicateBuilder.Or(user => user.Extra != null && user.Extra.Vk! == "id" + intId);
+            predicateBuilder = predicateBuilder.Or(user => user.ExternalLogins.Any(el => el.Key == intId.ToString()));
+        }
+
+        if (searchString.UnprefixNumber("id") is int vkIntId)
+        {
+            predicateBuilder = predicateBuilder.Or(user => user.Extra != null && user.Extra.Vk! == "id" + vkIntId);
+            matchByIdIsPerfect = true;
+        }
+
+        var results = await unitOfWork
+            .GetDbSet<User>()
+            .AsNoTracking()
+            .Include(u => u.Extra)
+            .Where(predicateBuilder)
+            .Take(30)
+            .ToListAsync();
 
         return results.Select(user =>
         {
-            var wasfoundById = user.UserId == idToFind;
+            var wasfoundById = user.UserId == idToFind || user.Extra?.Vk == idToFind.ToString();
             var description = new MarkdownString(wasfoundById
         ? WorldObjectProviderBase.GetFoundByIdDescription(user.UserId)
         : "");
