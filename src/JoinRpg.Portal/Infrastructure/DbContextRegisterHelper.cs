4d602557a842d6f84bd195a0995b94001600cbc4
Leonid Tsarev <leo@leotsarev.ru>
2024-08-06 20:13:42 +0300

    ExtractDbContextRegisterHelper

---
+++ src/JoinRpg.Portal/Infrastructure/DbContextRegisterHelper.cs
@@ -0,0 +1,32 @@
+using JoinRpg.Dal.JobService;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace JoinRpg.Portal.Infrastructure;
+
+public static class DbContextRegisterHelper
+{
+    public static void AddJoinEfCoreDbContext<TContext>(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment environment, string connectionStringName)
+    {
+        var connectionString = configuration.GetConnectionString(connectionStringName);
+
+        if (!string.IsNullOrWhiteSpace(connectionString))
+        {
+            services.AddDbContext<JobScheduleDataDbContext>(
+            options =>
+            {
+                options.UseNpgsql(connectionString);
+                options.EnableSensitiveDataLogging(environment.IsDevelopment());
+                options.EnableDetailedErrors(environment.IsDevelopment());
+            });
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services
+                .AddHealthChecks()
+                .AddNpgSql(
+                    connectionString,
+                    name: $"{connectionStringName}-db",
+                    failureStatus: HealthStatus.Degraded);
+        }
+    }
+}
