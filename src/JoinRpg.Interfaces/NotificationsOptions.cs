2acbdfc9ed7f5f9e2a5aa35698450f79666d0798
Leonid Tsarev <leo@leotsarev.ru>
2024-08-12 17:43:07 +0300

    Move joinrpg team const to NotificationsOptions

--- src/JoinRpg.Interfaces/NotificationsOptions.cs
+++ src/JoinRpg.Interfaces/NotificationsOptions.cs
@@ -1,8 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using JoinRpg.Interfaces.Email;
 
 namespace JoinRpg.Interfaces;
 public class NotificationsOptions
 {
     [Required]
     public string ServiceAccountEmail { get; set; } = null!;
+
+    [Required]
+    public string JoinRpgTeamName { get; set; } = null!;
+
+    public RecepientData ServiceRecepient => new(JoinRpgTeamName, ServiceAccountEmail);
 }
