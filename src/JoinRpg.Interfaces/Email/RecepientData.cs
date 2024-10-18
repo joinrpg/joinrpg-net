a7b0beaf55b67253c05ca5c9c8fb24c99303e1cc
Leonid Tsarev <leo@leotsarev.ru>
2024-10-18 17:51:50 +0300

    add new ctor for RecepientData

--- src/JoinRpg.Interfaces/Email/RecepientData.cs
+++ src/JoinRpg.Interfaces/Email/RecepientData.cs
@@ -1,4 +1,5 @@
 
+using JoinRpg.PrimitiveTypes;
 using JoinRpg.PrimitiveTypes.ProjectMetadata;
 
 namespace JoinRpg.Interfaces.Email;
@@ -9,8 +10,13 @@ public sealed class RecepientData(string displayName, string email, IReadOnlyDic
     public string Email { get; } = email ?? throw new ArgumentNullException(nameof(email));
     public IReadOnlyDictionary<string, string> RecipientSpecificValues { get; } = recipientSpecificValues ?? new Dictionary<string, string>();
 
-    public RecepientData(ProjectMasterInfo master) : this(master.Name.DisplayName, master.Email.Value)
+    public RecepientData(ProjectMasterInfo master) : this(master.Name, master.Email)
     {
 
+    }
+
+    public RecepientData(UserDisplayName displayName1, PrimitiveTypes.Email email, IReadOnlyDictionary<string, string>? recipientSpecificValues = null)
+        : this(displayName1.DisplayName, email.Value, recipientSpecificValues)
+    {
     }
 }
