162c4b0a80f2c1fe37d2b449cc2ce5e0a73bd979
Leonid Tsarev <leo@leotsarev.ru>
2024-10-31 11:54:45 +0300

    Add tests for ProblemTypeDisplay

---
+++ src/JoinRpg.WebPortal.Models.Test/ProblemTypeDisplayTest.cs
@@ -0,0 +1,22 @@
+using JoinRpg.Domain.Problems;
+using JoinRpg.Helpers;
+
+namespace JoinRpg.WebPortal.Models.Test;
+
+public class ProblemTypeDisplayTest
+{
+    [Theory]
+    [ClassData(typeof(EnumTheoryDataGenerator<ClaimProblemType>))]
+    public void EveryProblemTypeShouldHaveVisualization(ClaimProblemType problemType)
+    {
+        var problem = new ClaimProblem(problemType, ProblemSeverity.Error);
+        if (problemType.GetAttribute<ObsoleteAttribute>() is null)
+        {
+            _ = Should.NotThrow(() => new ProblemViewModel(problem));
+        }
+        else
+        {
+            _ = Should.Throw<KeyNotFoundException>(() => new ProblemViewModel(problem));
+        }
+    }
+}
