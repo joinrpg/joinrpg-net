a7fa809487065531c416cd4d60fa22ff4400c9b9
Leonid Tsarev <leo@leotsarev.ru>
2024-10-31 11:57:04 +0300

    Add inactive variant filter

--- src/JoinRpg.Domain/Problems/ClaimProblemType.cs
+++ src/JoinRpg.Domain/Problems/ClaimProblemType.cs
@@ -21,4 +21,5 @@ public enum ClaimProblemType
     FieldShouldNotHaveValue,
     NoParentGroup,
     GroupIsBroken,
+    InActiveVariant,
 }
--- src/JoinRpg.WebPortal.Models/ProblemViewModel.cs
+++ src/JoinRpg.WebPortal.Models/ProblemViewModel.cs
@@ -6,25 +6,26 @@ public class ProblemViewModel(ClaimProblem problem)
 {
     //Mass usage of Display(Name) turns out too slow...
     private static readonly Dictionary<ClaimProblemType, string> Types = new()
-{
-  {ClaimProblemType.NoResponsibleMaster, "Не назначен мастер"},
-  {ClaimProblemType.InvalidResponsibleMaster, "Неверный мастер"},
-  {ClaimProblemType.ClaimNeverAnswered, "Заявка без ответа"},
-  {ClaimProblemType.ClaimNoDecision, "По заявке нет решения"},
-  {ClaimProblemType.ClaimActiveButCharacterHasApprovedClaim, "Персонаж уже занят"},
-  {ClaimProblemType.FinanceModerationRequired, "Взнос на модерации"},
-  {ClaimProblemType.TooManyMoney, "Есть переплата по взносу"},
-  {ClaimProblemType.ClaimDiscussionStopped, "Обсуждение остановилось"},
-  {ClaimProblemType.NoCharacterOnApprovedClaim, "Нет персонажа у заявки"},
-  {ClaimProblemType.FeePaidPartially, "Взнос уплачен частично"},
-  {ClaimProblemType.UnApprovedClaimPayment, "Оплата в непринятой заявке"},
-  {ClaimProblemType.ClaimWorkStopped, "Работа по заявке остановлена"},
-  {ClaimProblemType.ClaimDontHaveTarget, "Заявка не привязана ни к чему"},
-  {ClaimProblemType.FieldIsEmpty, "Поле не заполнено"},
-  {ClaimProblemType.FieldShouldNotHaveValue, "Поле не должно быть заполнено для этой группы"},
-  {ClaimProblemType.NoParentGroup, "Персонаж не принадлежит ни к одной группе"},
-  {ClaimProblemType.GroupIsBroken, "Группа сломана"},
-};
+    {
+      {ClaimProblemType.NoResponsibleMaster, "Не назначен мастер"},
+      {ClaimProblemType.InvalidResponsibleMaster, "Неверный мастер"},
+      {ClaimProblemType.ClaimNeverAnswered, "Заявка без ответа"},
+      {ClaimProblemType.ClaimNoDecision, "По заявке нет решения"},
+      {ClaimProblemType.ClaimActiveButCharacterHasApprovedClaim, "Персонаж уже занят"},
+      {ClaimProblemType.FinanceModerationRequired, "Взнос на модерации"},
+      {ClaimProblemType.TooManyMoney, "Есть переплата по взносу"},
+      {ClaimProblemType.ClaimDiscussionStopped, "Обсуждение остановилось"},
+      {ClaimProblemType.NoCharacterOnApprovedClaim, "Нет персонажа у заявки"},
+      {ClaimProblemType.FeePaidPartially, "Взнос уплачен частично"},
+      {ClaimProblemType.UnApprovedClaimPayment, "Оплата в непринятой заявке"},
+      {ClaimProblemType.ClaimWorkStopped, "Работа по заявке остановлена"},
+      {ClaimProblemType.ClaimDontHaveTarget, "Заявка не привязана ни к чему"},
+      {ClaimProblemType.FieldIsEmpty, "Поле не заполнено"},
+      {ClaimProblemType.FieldShouldNotHaveValue, "Поле не должно быть заполнено для этой группы"},
+      {ClaimProblemType.NoParentGroup, "Персонаж не принадлежит ни к одной группе"},
+      {ClaimProblemType.GroupIsBroken, "Группа сломана"},
+      {ClaimProblemType.InActiveVariant, "Выбран удаленный вариант значения" },
+    };
 
     public string ProblemType { get; } = Types[problem.ProblemType];
 
