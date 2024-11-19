8ccdf63efc6bea7698b5a588517baf131077a6eb
Leonid Tsarev <leo@leotsarev.ru>
2024-11-19 12:34:47 +0300

    Перейти на использование SQL Server из compose, захардкоженные Design-Time миграции EF6

---
+++ src/JoinRpg.Dal.Impl/MyDbContextFactory.cs
@@ -0,0 +1,13 @@
+using System.Data.Entity.Infrastructure;
+
+namespace JoinRpg.Dal.Impl;
+public class MyDbContextFactory : IDbContextFactory<MyDbContext>
+{
+    // DbContextFactory используется только при миграциях, поэтому нормально что тут захардкожено.
+    MyDbContext IDbContextFactory<MyDbContext>.Create() => new(new DesignTimeConfig());
+
+    private class DesignTimeConfig : IJoinDbContextConfiguration
+    {
+        public string ConnectionString => "Data Source=127.0.0.1;User Id=sa;Password=MsSqlPass1!;MultipleActiveResultSets=True;Initial Catalog=joinrpg";
+    }
+}
