9a9ff5bfe20e44a6a929571108105493b7af8f01
Leonid Tsarev <leo@leotsarev.ru>
2024-11-21 14:36:05 +0300

    Видеть при подаче заявки как игрок

--- src/JoinRpg.PrimitiveTypes/AccessArguments.cs
+++ src/JoinRpg.PrimitiveTypes/AccessArguments.cs
@@ -1,25 +1,20 @@
 namespace JoinRpg.PrimitiveTypes;
 
 public record class AccessArguments(
+        /// <summary>
+        /// true if a user is logged in, he is a master of this game, he has access to the character or claim
+        /// </summary>
         bool MasterAccess,
+        /// <summary>
+        /// true if a user is logged in and he is assigned with the character
+        /// </summary>
         bool PlayerAccessToCharacter,
+        /// <summary>
+        /// true if a user is logged in and he is the owner of the claim
+        /// </summary>
         bool PlayerAccesToClaim,
         bool EditAllowed)
 {
-    /// <summary>
-    /// true if a user is logged in, he is a master of this game, he has access to the character or claim
-    /// </summary>
-    public bool MasterAccess { get; } = MasterAccess;
-
-    /// <summary>
-    /// true if a user is logged in and he is assigned with the character
-    /// </summary>
-    public bool PlayerAccessToCharacter { get; } = PlayerAccessToCharacter;
-
-    /// <summary>
-    /// true if a user is logged in and he is the owner of the claim
-    /// </summary>
-    public bool PlayerAccesToClaim { get; } = PlayerAccesToClaim;
 
     /// <summary>
     /// true if there is master or player access to the character
@@ -30,6 +25,8 @@ public record class AccessArguments(
     /// true, if there is master or player access to the claim
     /// </summary>
     public bool AnyAccessToClaim { get; } = PlayerAccesToClaim || PlayerAccesToClaim;
+
+    public AccessArguments WithoutMasterAccess() => this with { MasterAccess = false };
 
     public override string ToString() => $"AccessArguments(MasterAccess:{MasterAccess}, CharacterAccess:{PlayerAccessToCharacter}, PlayerAccesToClaim:{PlayerAccesToClaim}";
 }
