21d3b995de149dd53fa44c90165e66ad769021a4
Leonid Tsarev <leo@leotsarev.ru>
2024-11-29 21:09:18 +0300

    Split IProjectEntity interface

--- src/JoinRpg.DataModel/Interfaces/IProjectEntity.cs
+++ src/JoinRpg.DataModel/Interfaces/IProjectEntity.cs
@@ -1,11 +1,18 @@
 using JoinRpg.Helpers;
+using JoinRpg.PrimitiveTypes;
 
 namespace JoinRpg.DataModel;
 
-public interface IProjectEntity : IOrderableEntity
+public interface IProjectEntity : IProjectEntityWithId
 {
 
     Project Project { get; }
+}
+
+public interface IProjectEntityWithId : IOrderableEntity
+{
 
     int ProjectId { get; }
+
+    ProjectIdentification ProjectIdentification => new ProjectIdentification(ProjectId);
 }
