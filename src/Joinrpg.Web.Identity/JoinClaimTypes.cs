f9a81c13a83d5ffa83bca7b364b4363dcd76abbf
Leonid Tsarev <leo@leotsarev.ru>
2024-12-16 18:01:25 +0300

    Удалить остатки попыток перенести доступ к проектам в claim (#2960)

--- src/Joinrpg.Web.Identity/JoinClaimTypes.cs
+++ src/Joinrpg.Web.Identity/JoinClaimTypes.cs
@@ -10,6 +10,4 @@ public static class JoinClaimTypes
     /// Id of Avatar
     /// </summary>
     public const string AvatarId = "joinrpg.AvatarId";
-
-    public const string ProjectPrefix = "joinrpg.MasterOf__";
 }
