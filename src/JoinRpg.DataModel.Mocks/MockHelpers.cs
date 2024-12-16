b4b01b2fa2b96530712a9c5af226b890228bd313
Leonid Tsarev <leo@leotsarev.ru>
2024-12-16 19:29:03 +0300

    Улучшить тесты полей

--- src/JoinRpg.DataModel.Mocks/MockHelpers.cs
+++ src/JoinRpg.DataModel.Mocks/MockHelpers.cs
@@ -1,7 +1,7 @@
-using JoinRpg.DataModel;
+using JoinRpg.PrimitiveTypes;
 
 static internal class MockHelpers
 {
     public static int GetNextId<T>(this IEnumerable<T> entitySet)
-        where T : IProjectEntity => entitySet.Select(f => f.Id).DefaultIfEmpty().Max() + 1;
+        where T : IProjectEntityWithId => entitySet.Select(f => f.Id).DefaultIfEmpty().Max() + 1;
 }
