94fec69a4b2bac05d9199225f5a4536f3ba334e3
Leonid Tsarev <leo@leotsarev.ru>
2025-02-17 18:53:35 +0300

    ProjectId теперь распознаются как параметры контроллеров

--- src/JoinRpg.PrimitiveTypes/ProjectIdentification.cs
+++ src/JoinRpg.PrimitiveTypes/ProjectIdentification.cs
@@ -1,6 +1,8 @@
+using System.Diagnostics.CodeAnalysis;
+
 namespace JoinRpg.PrimitiveTypes;
 
-public record ProjectIdentification(int Value) : SingleValueType<int>(Value), ILinkable
+public record ProjectIdentification(int Value) : SingleValueType<int>(Value), ILinkable, ISpanParsable<ProjectIdentification>
 {
     LinkType ILinkable.LinkType => LinkType.Project;
 
@@ -11,4 +13,33 @@ public record ProjectIdentification(int Value) : SingleValueType<int>(Value), IL
     public override string ToString() => base.ToString();
 
     public static ProjectIdentification? FromOptional(int? value) => value is int id ? new(id) : null;
+    public static ProjectIdentification Parse(string value, IFormatProvider? provider) => Parse(value.AsSpan(), provider);
+
+    public static bool TryParse(string? value, IFormatProvider? provider, [MaybeNullWhen(false)] out ProjectIdentification result) => TryParse(value.AsSpan(), provider, out result);
+
+    public static ProjectIdentification Parse(ReadOnlySpan<char> value, IFormatProvider? provider)
+        => TryParse(value, provider, out var result) ? result : throw new ArgumentException("Could not parse supplied value.", nameof(value));
+    public static bool TryParse(ReadOnlySpan<char> value, IFormatProvider? provider, [MaybeNullWhen(false)] out ProjectIdentification result)
+    {
+        var val = value.Trim();
+        if (val.StartsWith(nameof(ProjectIdentification)))
+        {
+            val = val[nameof(ProjectIdentification).Length..];
+        }
+        if (val.StartsWith("("))
+        {
+            val = val[1..];
+        }
+        if (val.EndsWith(")"))
+        {
+            val = val[..^1];
+        }
+        if (int.TryParse(val, provider, out var id))
+        {
+            result = new ProjectIdentification(id);
+            return true;
+        }
+        result = new ProjectIdentification(0);
+        return false;
+    }
 }
