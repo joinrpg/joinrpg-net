e508c3caffbf39466025489bd7616a0cafb121a2
Leonid Tsarev <leo@leotsarev.ru>
2025-02-22 13:00:10 +0300

    форматирование

--- src/JoinRpg.Dal.Impl/Migrations/202408151446243_RemoveCharacterNameLegacyMode.cs
+++ src/JoinRpg.Dal.Impl/Migrations/202408151446243_RemoveCharacterNameLegacyMode.cs
@@ -1,17 +1,16 @@
-namespace JoinRpg.Dal.Impl.Migrations
+namespace JoinRpg.Dal.Impl.Migrations;
+
+using System.Data.Entity.Migrations;
+
+public partial class RemoveCharacterNameLegacyMode : DbMigration
 {
-    using System.Data.Entity.Migrations;
-
-    public partial class RemoveCharacterNameLegacyMode : DbMigration
+    public override void Up()
     {
-        public override void Up()
-        {
-            DropColumn("dbo.ProjectDetails", "CharacterNameLegacyMode");
-        }
+        DropColumn("dbo.ProjectDetails", "CharacterNameLegacyMode");
+    }
 
-        public override void Down()
-        {
-            AddColumn("dbo.ProjectDetails", "CharacterNameLegacyMode", c => c.Boolean(nullable: false));
-        }
+    public override void Down()
+    {
+        AddColumn("dbo.ProjectDetails", "CharacterNameLegacyMode", c => c.Boolean(nullable: false));
     }
 }
