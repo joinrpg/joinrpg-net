5f228c601716f2cd92055a4fec5a7523fd3f58de
Leonid Tsarev <leo@leotsarev.ru>
2025-03-25 09:24:24 +0300

    Поправить CountHelper для корректного склонения и удалить лишние копии (#3147)
    * Поправить CountHelper для корректного склонения
    
    * Удалить кучу копий DisplayCount

--- src/JoinRpg.Helpers/CountHelper.cs
+++ src/JoinRpg.Helpers/CountHelper.cs
@@ -4,7 +4,16 @@ public static class CountHelper
 {
     public static string DisplayCount(int count, string single, string multi1, string multi2)
     {
-        var selected = count == 0 ? multi2 : (count == 1 ? single : (count < 5 ? multi1 : multi2));
+        var absCount = (uint)Math.Abs(count);
+        var mod = absCount % 10;
+        var selected = mod switch
+        {
+            0 => multi2,
+            1 when absCount % 100 == 11 => multi2,
+            1 => single,
+            > 1 and < 5 => multi1,
+            >= 5 => multi2,
+        };
         return count + " " + @selected;
     }
 }
