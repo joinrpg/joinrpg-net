824af72490e6b395f54ac2bd495e242d33295b00
Leonid Tsarev <leo@leotsarev.ru>
2025-04-04 13:46:20 +0300

    Конфигурация КогдаИгры (#3177)

--- src/JoinRpg.Services.Interfaces/Integrations/KogdaIgra/KogdaIgraOptions.cs
+++ src/JoinRpg.Services.Interfaces/Integrations/KogdaIgra/KogdaIgraOptions.cs
@@ -2,5 +2,5 @@ namespace JoinRpg.Services.Interfaces.Integrations.KogdaIgra;
 
 public class KogdaIgraOptions
 {
-    public Uri HostName { get; set; } = new Uri("https://dev.kogda-igra.ru/");
+    public Uri HostName { get; set; } = null!;
 }
