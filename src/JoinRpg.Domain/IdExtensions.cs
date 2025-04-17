88ce39c60ebae548149c2b24b0ba762403219277
Leonid Tsarev <leo@leotsarev.ru>
2025-04-18 00:09:34 +0300

    Не копируются значения выборов в персонаже при копировании проекта fixes #3284

--- src/JoinRpg.Domain/IdExtensions.cs
+++ src/JoinRpg.Domain/IdExtensions.cs
@@ -7,4 +7,6 @@ public static class IdExtensions
     public static CharacterIdentification GetId(this Character character) => new CharacterIdentification(character.ProjectId, character.CharacterId);
 
     public static CharacterGroupIdentification GetId(this CharacterGroup group) => new CharacterGroupIdentification(group.ProjectId, group.CharacterGroupId);
+
+    public static ProjectFieldVariantIdentification GetId(this ProjectFieldDropdownValue variant) => new(variant.ProjectId, variant.ProjectFieldId, variant.ProjectFieldDropdownValueId);
 }
