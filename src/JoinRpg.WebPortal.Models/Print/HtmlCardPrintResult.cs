fcacf6b55e452c454d8d02fe773593761a658e69
Leonid Tsarev <leo@leotsarev.ru>
2025-04-19 16:51:59 +0300

    Убрать неиспользуемый background

--- src/JoinRpg.WebPortal.Models/Print/HtmlCardPrintResult.cs
+++ src/JoinRpg.WebPortal.Models/Print/HtmlCardPrintResult.cs
@@ -2,18 +2,15 @@ namespace JoinRpg.Web.Models.Print;
 
 public class HtmlCardPrintResult
 {
-    public HtmlCardPrintResult(string html, CardSize cardSize, string? backgroundUrl = null)
+    public HtmlCardPrintResult(string html, CardSize cardSize)
     {
         Html = html;
         CardSize = cardSize;
-        BackgroundUrl = backgroundUrl;
     }
 
     public string Html { get; }
 
     public CardSize CardSize { get; }
-
-    public string? BackgroundUrl { get; }
 }
 
 public enum CardSize
