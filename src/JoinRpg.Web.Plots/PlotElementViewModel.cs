37eb28a128e6d407e854cd734fa7c02614ef5a42
Leonid Tsarev <leo@leotsarev.ru>
2025-04-29 15:54:21 +0300

    Поправить баг с рендерингом плоским списком (#3337)

--- src/JoinRpg.Web.Plots/PlotElementViewModel.cs
+++ src/JoinRpg.Web.Plots/PlotElementViewModel.cs
@@ -1,41 +1,9 @@
 using Microsoft.AspNetCore.Components;
+using static JoinRpg.Web.Plots.PlotStatus;
 
 namespace JoinRpg.Web.Plots;
 
-public class PlotElementViewModel
+public record class PlotRenderedTextViewModel(MarkupString Content, MarkupString? Todo, PlotVersionIdentification PlotVersionId, PlotStatus? PlotStatus, TargetsInfo? Target)
 {
-    public PlotStatus? Status { get; }
-    public MarkupString Content { get; }
-    public MarkupString? TodoField { get; }
-    public PlotElementIdentification PlotElementId { get; }
-
-    public bool ShowEditControls { get; }
-
-    public bool First { get; set; }
-    public bool Last { get; set; }
-
-    public TargetsInfo? Target { get; }
-
-    public PlotRenderedTextViewModel PlotText { get; }
-
-    public PlotElementViewModel(CharacterIdentification? characterId,
-        PlotRenderedTextViewModel plotText,
-        bool showEditControls)
-    {
-        ArgumentNullException.ThrowIfNull(plotText);
-
-        Content = plotText.Content;
-        TodoField = plotText.Todo;
-        ShowEditControls = showEditControls;
-        PlotElementId = plotText.PlotVersionId.PlotElementId;
-        Status = plotText.PlotStatus;
-        Target = plotText.Target;
-        CharacterId = characterId;
-    }
-
-    public CharacterIdentification? CharacterId { get; }
-
-    public bool HasWorkTodo => !string.IsNullOrWhiteSpace(TodoField?.Value) || Status == PlotStatus.InWork || Status == PlotStatus.HasNewVersion;
+    public bool HasWorkTodo => !string.IsNullOrWhiteSpace(Todo?.Value) || PlotStatus == InWork || PlotStatus == HasNewVersion;
 }
-
-public record class PlotRenderedTextViewModel(MarkupString Content, MarkupString? Todo, PlotVersionIdentification PlotVersionId, PlotStatus? PlotStatus, TargetsInfo? Target);
