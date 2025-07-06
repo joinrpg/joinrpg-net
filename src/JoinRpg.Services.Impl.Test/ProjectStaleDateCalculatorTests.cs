3299fe05e7bb5595f4382cabd12a0359e56ff97e
Leonid Tsarev <leo@leotsarev.ru>
2025-07-06 20:20:13 +0300

    Предупреждать 3 раза перед закрытием проекта (#3470)

--- src/JoinRpg.Services.Impl.Test/ProjectStaleDateCalculatorTests.cs
+++ src/JoinRpg.Services.Impl.Test/ProjectStaleDateCalculatorTests.cs
@@ -4,23 +4,43 @@ namespace JoinRpg.Services.Impl.Test;
 public class ProjectStaleDateCalculatorTests
 {
     [Fact]
-    public void InFutureShouldBeSixMonthLater()
+    public void ShouldNotCloseNotStale()
+        => ProjectStaleDateCalculator.CalculateStaleStatus(lastUpdated: new DateOnly(2025, 07, 06), today: new DateOnly(2025, 08, 06))
+        .ShouldBe((ProjectStaleStatus.NotStale, DateOnly.MaxValue));
+
+    [Fact]
+    public void ShouldCloseAfterSevenMonthsExact()
     {
-        var lastUpdated = new DateOnly(2025, 1, 1);
-        ProjectStaleDateCalculator.CalculateCloseDate(lastUpdated).ShouldBe(new DateOnly(2025, 8, 1));
+        var today = new DateOnly(2025, 08, 01);
+        ProjectStaleDateCalculator
+            .CalculateStaleStatus(lastUpdated: new DateOnly(2025, 1, 1), today: today)
+            .ShouldBe((ProjectStaleStatus.StaleCloseToday, today));
     }
 
     [Fact]
-    public void ShouldBeAfterNovebmer2024()
+    public void ShouldNotCloseAfterSixMonthsWhenNotExact()
     {
-        var lastUpdated = new DateOnly(2021, 1, 1);
-        ProjectStaleDateCalculator.CalculateCloseDate(lastUpdated).ShouldBe(new DateOnly(2024, 11, 1));
+        var today = new DateOnly(2025, 09, 02);
+        ProjectStaleDateCalculator
+            .CalculateStaleStatus(lastUpdated: new DateOnly(2025, 1, 1), today: today)
+            .ShouldBe((ProjectStaleStatus.StaleWarnOtherTime, new DateOnly(2025, 10, 01)));
     }
 
     [Fact]
-    public void Scrackanobaza()
+    public void ShouldWarnWhenSameDayAndStale()
     {
-        var lastUpdated = new DateOnly(2024, 7, 16);
-        ProjectStaleDateCalculator.CalculateCloseDate(lastUpdated).ShouldBe(new DateOnly(2025, 2, 16));
+        var today = new DateOnly(2025, 06, 01);
+        ProjectStaleDateCalculator
+            .CalculateStaleStatus(lastUpdated: new DateOnly(2025, 01, 1), today: today)
+            .ShouldBe((ProjectStaleStatus.StaleWarnToday, new DateOnly(2025, 08, 01)));
+    }
+
+    [Fact]
+    public void ShouldWarnAfter4Months()
+    {
+        var today = new DateOnly(2025, 07, 06);
+        ProjectStaleDateCalculator
+            .CalculateStaleStatus(lastUpdated: new DateOnly(2025, 02, 16), today: today)
+            .ShouldBe((ProjectStaleStatus.StaleWarnOtherTime, new DateOnly(2025, 09, 16)));
     }
 }
