0fc955b2742212a8716cbdf31705c34de892dcc4
Vlad Kiselev <spbipp@gmail.com>
2025-07-12 15:16:28 +0300

    Изменен порядок юзингов

--- src/Joinrpg.Dal.Migrate/Program.cs
+++ src/Joinrpg.Dal.Migrate/Program.cs
@@ -1,10 +1,10 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using JoinRpg.Dal.JobService;
 using JoinRpg.Dal.Migrate.EfCore;
 using JoinRpg.Dal.Migrate.Ef6;
 using JoinRpg.Portal.Infrastructure;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace JoinRpg.Dal.Migrate;
 
