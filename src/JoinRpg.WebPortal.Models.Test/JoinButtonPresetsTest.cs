0ea2b2b964fd2bcfb795077ac3ffcaf5fac188cb
Leonid Tsarev <leo@leotsarev.ru>
2025-07-25 23:55:48 +0300

    Поправить юнит-тест

--- src/JoinRpg.WebPortal.Models.Test/JoinButtonPresetsTest.cs
+++ src/JoinRpg.WebPortal.Models.Test/JoinButtonPresetsTest.cs
@@ -4,20 +4,21 @@ namespace JoinRpg.WebPortal.Models.Test;
 
 public class JoinButtonPresetsTest
 {
-    /*
     [Theory]
     [ClassData(typeof(EnumTheoryDataGenerator<ButtonPreset>))]
     public void PresetsShouldBeRecognized(ButtonPreset buttonPreset)
     {
+
         var preset = JoinButton.Presets[buttonPreset];
         if (buttonPreset == ButtonPreset.None)
         {
-            preset.ShouldBeNull();
+            preset.Normal.Label.ShouldBeNull();
+            preset.Normal.Icon.ShouldBeNull();
         }
         else
         {
-            preset.ShouldNotBeNull();
+            preset.Normal.Label.ShouldNotBeNull();
+            //preset.Normal.Icon.ShouldNotBeNull();
         }
     }
-*/
 }
