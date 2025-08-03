6ea8ab18bdb0d8647264227184b4dd5c82b13016
Leonid Tsarev <leo@leotsarev.ru>
2025-08-03 16:38:57 +0300

    Почистить CharacterListItemViewModelExporter (#3518)

--- src/JoinRpg.WebPortal.Models/Exporters/CharacterListItemViewModelExporter.cs
+++ src/JoinRpg.WebPortal.Models/Exporters/CharacterListItemViewModelExporter.cs
@@ -1,24 +1,21 @@
 using JoinRpg.PrimitiveTypes.ProjectMetadata;
 using JoinRpg.Services.Interfaces;
+using JoinRpg.Web.Models.Characters;
 
 namespace JoinRpg.Web.Models.Exporters;
 
-public class CharacterListItemViewModelExporter : CharacterListExporterBase
+public class CharacterListItemViewModelExporter(ProjectInfo projectInfo, IUriService uriService) : CustomExporter<CharacterListItemViewModel>(uriService)
 {
-
-    public CharacterListItemViewModelExporter(ProjectInfo projectInfo, IUriService uriService) :
-      base(projectInfo, uriService)
-    {
-    }
-
     public override IEnumerable<ITableColumn> ParseColumns()
     {
-        foreach (var tableColumn in BasicColumns())
-        {
-            yield return tableColumn;
-        }
+        yield return StringColumn(x => x.Name);
+        yield return UriColumn(x => x, "Персонаж");
+        yield return EnumColumn(x => x.BusyStatus);
+        yield return StringListColumn(x => x.Groups.ParentGroups.Select(g => g.Name), "Группы");
+        yield return UriListColumn(x => x.Groups.ParentGroups);
+        yield return ShortUserColumn(x => x.Responsible);
 
-        foreach (var projectField in ProjectInfo.SortedFields.Where(f => f.CanHaveValue))
+        foreach (var projectField in projectInfo.SortedFields.Where(f => f.CanHaveValue))
         {
             yield return FieldColumn(projectField, x => x.Fields);
         }
