4ddf885cab9737e76c04b083c22426124b12604e
Leonid Tsarev <leo@leotsarev.ru>
2025-08-17 18:17:01 +0300

    Добавить ProjectLink

--- src/JoinRpg.Blazor.Client/UriLocatorExtensions.cs
+++ src/JoinRpg.Blazor.Client/UriLocatorExtensions.cs
@@ -1,11 +1,14 @@
 using JoinRpg.Web.ProjectCommon;
+using JoinRpg.Web.ProjectCommon.Projects;
 using JoinRpg.WebComponents;
 
 namespace JoinRpg.Blazor.Client;
 
 public static class UriLocatorExtensions
 {
-    private class UriLocator : IUriLocator<UserLinkViewModel>, IUriLocator<CharacterGroupLinkSlimViewModel>, IUriLocator<CharacterLinkSlimViewModel>
+    private class UriLocator :
+        IUriLocator<UserLinkViewModel>, IUriLocator<CharacterGroupLinkSlimViewModel>, IUriLocator<CharacterLinkSlimViewModel>,
+        IUriLocator<ProjectLinkViewModel>
     {
         Uri IUriLocator<UserLinkViewModel>.GetUri(UserLinkViewModel target)
         {
@@ -13,7 +16,7 @@ public static class UriLocatorExtensions
             {
                 throw new InvalidOperationException("Should not have url of hidden");
             }
-            return new($"/user/{target.UserId}");
+            return new($"/user/{target.UserId}", UriKind.Relative);
         }
 
         // TODO implement for Blazor. Added so we will have nice exception instead of bla-bla not resolved.
@@ -23,11 +26,18 @@ public static class UriLocatorExtensions
 
         //end of TODO
 
+        Uri IUriLocator<ProjectLinkViewModel>.GetUri(ProjectLinkViewModel target) => new($"/{target.ProjectId.Value}/home", UriKind.Relative);
+
+
+
     }
     public static IServiceCollection AddUriLocator(this IServiceCollection serviceCollection)
     {
         var locator = new UriLocator();
         serviceCollection.AddSingleton<IUriLocator<UserLinkViewModel>>(locator);
+        serviceCollection.AddSingleton<IUriLocator<ProjectLinkViewModel>>(locator);
+        serviceCollection.AddSingleton<IUriLocator<CharacterGroupLinkSlimViewModel>>(locator);
+        serviceCollection.AddSingleton<IUriLocator<CharacterLinkSlimViewModel>>(locator);
         return serviceCollection;
     }
 }
