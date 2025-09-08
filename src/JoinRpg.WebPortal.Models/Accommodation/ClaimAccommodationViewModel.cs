1b9144adb0d0ad73de9993e2794a56f1b23a4e93
Leonid Tsarev <leo@leotsarev.ru>
2025-09-08 12:38:17 +0300

    Поправить проблему с HTML символами в описании поселения

--- src/JoinRpg.WebPortal.Models/Accommodation/ClaimAccommodationViewModel.cs
+++ src/JoinRpg.WebPortal.Models/Accommodation/ClaimAccommodationViewModel.cs
@@ -1,5 +1,4 @@
 using JoinRpg.DataModel;
-using JoinRpg.Web.Models.Characters;
 
 namespace JoinRpg.Web.Models.Accommodation;
 
@@ -7,11 +6,10 @@ public class ClaimAccommodationViewModel
 {
     public int ClaimId { get; set; }
     public int ProjectId { get; set; }
-    public IEnumerable<ProjectAccommodationType> AvailableAccommodationTypes { get; set; }
-    public IEnumerable<AccommodationPotentialNeighbors> PotentialNeighbors { get; set; }
-    public IEnumerable<AccommodationInvite> IncomingInvite { get; set; }
-    public IEnumerable<AccommodationInvite> OutgoingInvite { get; set; }
-    public CharacterNavigationViewModel Navigation;
+    public required IEnumerable<ProjectAccommodationType> AvailableAccommodationTypes { get; set; }
+    public required IEnumerable<AccommodationPotentialNeighbors> PotentialNeighbors { get; set; }
+    public required IEnumerable<AccommodationInvite> IncomingInvite { get; set; }
+    public required IEnumerable<AccommodationInvite> OutgoingInvite { get; set; }
     public AccommodationRequest? AccommodationRequest { get; set; }
     public bool AccommodationEnabledForClaim { get; set; }
 }
