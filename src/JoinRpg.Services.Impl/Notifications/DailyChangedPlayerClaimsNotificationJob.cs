3a3b8b9c59edc182c0d25b0ae66bd99e54709efb
Leonid Tsarev <leo@leotsarev.ru>
2025-09-20 11:33:47 +0300

    Добавить метрики телеграм джобе

--- src/JoinRpg.Services.Impl/Notifications/DailyChangedPlayerClaimsNotificationJob.cs
+++ src/JoinRpg.Services.Impl/Notifications/DailyChangedPlayerClaimsNotificationJob.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.Metrics;
 using JoinRpg.Data.Interfaces;
 using JoinRpg.Data.Interfaces.Claims;
 using JoinRpg.Interfaces;
@@ -16,39 +17,65 @@ internal class DailyChangedPlayerClaimsNotificationJob(
     IUriService uriService
     ) : IDailyJob
 {
+    private static readonly Meter meter = new("JoinRpg");
+    private static readonly Counter<int> playersWithChangedClaimsHistogram = meter.CreateCounter<int>("joinrpg.notification.players_with_changed_claims");
+    private static readonly Counter<int> playersWithChangedClaimsAndTelegramHistogram = meter.CreateCounter<int>("joinrpg.notification.players_with_changed_claims_with_telegram");
+    private static readonly Counter<int> playersWithChangedClaimsAndTelegramEnabledHistogram = meter.CreateCounter<int>("joinrpg.notification.players_with_changed_claims_with_telegram_enabled");
+
+    private int playersWithChangedClaimsAndTelegram, playersWithChangedClaimsAndTelegramEnabled;
+
     public async Task RunOnce(CancellationToken cancellationToken)
     {
         var sinceTime = DateTime.UtcNow.AddHours(-24).AddMinutes(-20);
         // Интервалы будут частично перекрываться, чтобы это предотвратить, нужно записывать какие конкретно изменения были отправлены
         var updates = await claimsRepository.GetUpdatedClaimsSince(sinceTime);
         logger.LogInformation("Найдено {updatedClaimsCount} обновлений {since}", updates.Count, sinceTime);
+        playersWithChangedClaimsHistogram.Add(updates.Count);
         foreach (var userUpdates in updates.GroupBy(u => u.UserId))
         {
-            var user = await userRepository.GetUserInfo(userUpdates.Key);
-            var userUpdatesList = userUpdates.ToList();
-            if (user is null)
-            {
-                logger.LogError("Не нашли пользователя {userId}", userUpdates.Key);
-                continue;
-            }
-            logger.LogInformation("У пользователя {userId} {userName} было {userUpdatedClaimsCount} обновлений в заявках. Телеграм подключен: {telegramConnected}. Обновления включены: {telegramEnabled}",
-                user.UserId, user.DisplayName, userUpdatesList.Count, user.Social.TelegramId is not null, user.NotificationSettings.TelegramDigestEnabled);
+            await SendNotificationForPlayer(userUpdates);
+        }
+        playersWithChangedClaimsAndTelegramHistogram.Add(playersWithChangedClaimsAndTelegram);
+        playersWithChangedClaimsAndTelegramEnabledHistogram.Add(playersWithChangedClaimsAndTelegramEnabled);
+    }
 
-            if (user.Social.TelegramId is null || !user.NotificationSettings.TelegramDigestEnabled)
+    private async Task SendNotificationForPlayer(IGrouping<PrimitiveTypes.UserIdentification, UpdatedClaimDto> userUpdates)
+    {
+        var user = await userRepository.GetUserInfo(userUpdates.Key);
+        var userUpdatesList = userUpdates.ToList();
+        if (user is null)
+        {
+            logger.LogError("Не нашли пользователя {userId}", userUpdates.Key);
+            return;
+        }
+        logger.LogInformation("У пользователя {userId} {userName} было {userUpdatedClaimsCount} обновлений в заявках. Телеграм подключен: {telegramConnected}. Обновления включены: {telegramEnabled}",
+            user.UserId, user.DisplayName, userUpdatesList.Count, user.Social.TelegramId is not null, user.NotificationSettings.TelegramDigestEnabled);
+
+        if (user.Social.TelegramId is not null)
+        {
+            playersWithChangedClaimsAndTelegram++;
+            if (user.NotificationSettings.TelegramDigestEnabled)
+            {
+                playersWithChangedClaimsAndTelegramEnabled++;
+            }
+            else
             {
                 return;
             }
+        }
+        else
+        {
+            return;
+        }
 
-            var message = $@"Привет, {user.DisplayName.DisplayName}!
+        var message = $@"Привет, {user.DisplayName.DisplayName}!
 Твои заявки на игры были обновлены за последний день:
 {string.Join("\n", userUpdatesList.Select(update => $"<a href=\"{GetClaimUri(update).AbsoluteUri}\">{update.CharacterName}</a>  на игру {update.ProjectName.Value}"))}
 ";
 
-            // TODO Если ты не хочешь получать это сообщение, отключи его в настройках;
+        // TODO Если ты не хочешь получать это сообщение, отключи его в настройках;
 
-            await telegramNotificationService.SendTelegramNotification(user.Social.TelegramId, new TelegramHtmlString(message));
-
-        }
+        await telegramNotificationService.SendTelegramNotification(user.Social.TelegramId, new TelegramHtmlString(message));
     }
 
     private Uri GetClaimUri(UpdatedClaimDto update) => uriService.GetUri(update.ClaimId);
