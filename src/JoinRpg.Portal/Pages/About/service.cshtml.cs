57e516de623a21672e9d4337055c9bce3828b4dd
Leonid Tsarev <leo@leotsarev.ru>
2025-09-29 20:28:29 +0300

    Добавить ИНН (#3633)

---
+++ src/JoinRpg.Portal/Pages/About/service.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JoinRpg.Portal.Pages.About
+{
+    public class serviceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
