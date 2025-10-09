ecfa488f33871965ab76115322c414b97d2201bc
Leonid Tsarev <leo@leotsarev.ru>
2025-10-09 21:09:12 +0300

    Поправить поселение (#3639)

--- src/JoinRpg.Domain/AccommodationExtensions.cs
+++ src/JoinRpg.Domain/AccommodationExtensions.cs
@@ -8,4 +8,35 @@ public static class AccommodationExtensions
         room.Inhabitants.SelectMany(i => i.Subjects);
 
     public static bool IsOccupied(this ProjectAccommodation pa) => pa.Inhabitants.Any();
+
+    public static int GetRoomFreeSpace(this AccommodationRequest accommodationRequest1)
+    {
+        if (accommodationRequest1.Accommodation is ProjectAccommodation accommodation)
+        {
+            return accommodation.GetRoomFreeSpace();
+        }
+        else
+        {
+            return accommodationRequest1.AccommodationType.Capacity - accommodationRequest1.Subjects.Count;
+        }
+    }
+
+    public static List<User> GetClaimNeighbours(this Claim claim)
+    {
+        if (claim.AccommodationRequest is AccommodationRequest accommodationRequest)
+        {
+            if (claim.AccommodationRequest.Accommodation is ProjectAccommodation accommodation)
+            {
+                return [.. accommodation.Inhabitants.SelectMany(i => i.Subjects).Where(s => s.ClaimId != claim.ClaimId).Select(c => c.Player)];
+            }
+            else
+            {
+                return [.. accommodationRequest.Subjects.Where(s => s.ClaimId != claim.ClaimId).Select(c => c.Player)];
+            }
+        }
+        else
+        {
+            return [];
+        }
+    }
 }
