675109ff15c6b04eb73bfab9a5f7256ce9a269fc
Leonid Tsarev <leo@leotsarev.ru>
2025-10-20 20:45:40 +0300

    Убрать слеш в начале путей в Excel для красоты

--- src/JoinRpg.Services.Export/BackEnds/ClosedXmlExcelBackend.cs
+++ src/JoinRpg.Services.Export/BackEnds/ClosedXmlExcelBackend.cs
@@ -61,7 +61,7 @@ internal partial class ClosedXmlExcelBackend : IGeneratorBackend
                 _ = xlCell.SetValue(time);
                 break;
             case Uri uri:
-                _ = xlCell.SetValue(uri.PathAndQuery);
+                _ = xlCell.SetValue(uri.PathAndQuery.TrimStart('/'));
                 xlCell.GetHyperlink().ExternalAddress = uri;
                 break;
             default:
