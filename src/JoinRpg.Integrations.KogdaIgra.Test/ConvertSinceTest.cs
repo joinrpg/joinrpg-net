9ce69fefcc6c21a208a65a8ead91133fa7c5462d
Leonid Tsarev <leo@leotsarev.ru>
2025-10-28 20:43:17 +0300

    Поправить конвертацию в timestamp КогдаИгры (#3703)

---
+++ src/JoinRpg.Integrations.KogdaIgra.Test/ConvertSinceTest.cs
@@ -0,0 +1,11 @@
+namespace JoinRpg.Integrations.KogdaIgra.Test;
+public class ConvertSinceTest
+{
+    [Fact]
+    public void EnsureCorrectConversion()
+    {
+        var time = new DateTimeOffset(2025, 10, 28, 19, 58, 33, TimeSpan.FromHours(3));
+
+        KogdaIgraApiClient.ConvertToKogdaIgraTimeStamp(time).ShouldBe(1761670713);
+    }
+}
