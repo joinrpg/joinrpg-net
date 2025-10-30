757e35635a5bdbb474bf91048cefdc20760ce614
Leonid Tsarev <leo@leotsarev.ru>
2025-10-30 11:11:42 +0300

    Починить экспорт бюджета в гуглдоки (#3708)

--- src/JoinRpg.WebPortal.Models/Exporters/MoneySummaryByMasterExporter.cs
+++ src/JoinRpg.WebPortal.Models/Exporters/MoneySummaryByMasterExporter.cs
@@ -2,25 +2,25 @@ using JoinRpg.Services.Interfaces;
 
 namespace JoinRpg.Web.Models.Exporters;
 
-public class MoneySummaryByMasterExporter : CustomExporter<MasterBalanceViewModel>
+public class MoneySummaryByMasterExporter(IUriService uriService) : CustomExporter<MasterBalanceViewModel>(uriService)
 {
-    public MoneySummaryByMasterExporter(IUriService uriService) : base(uriService)
-    {
-    }
-
     public override IEnumerable<ITableColumn> ParseColumns()
     {
+        // Порядок полей тут важен, так как гуглдоки у людей опираются скорее всего на порядок колонок
         yield return ShortUserColumn(x => x.Master, "Мастер");
         yield return ComplexElementMemberColumn(x => x.Master, x => x.Email);
         yield return ComplexElementMemberColumn(x => x.Master,
             x => x.Extra,
             x => x.PhoneNumber,
             "Телефон");
+        yield return ComplexElementMemberColumn(x => x.Master, x => x.Extra, x => x.Telegram, "Telegram");
+        // Выше должно быть ровно 4 поля с какими-то данными профиля мастера
         yield return IntColumn(x => x.Total);
         yield return IntColumn(x => x.ReceiveBalance);
         yield return IntColumn(x => x.SendBalance);
         yield return IntColumn(x => x.FeeBalance);
         yield return IntColumn(x => x.ModerationBalance);
         yield return IntColumn(x => x.ExpensesBalance);
+        // выше должно быть 6 полей с цифирками ровно в таком порядке
     }
 }
