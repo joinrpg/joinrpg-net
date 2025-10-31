984b20ef2284e45da23b2a4356272689019f43f3
Leonid Tsarev <leo@leotsarev.ru>
2025-10-31 11:09:57 +0300

    Поправить баг с падением (#3715)

--- src/JoinRpg.Services.Impl/Projects/ProjectStaleDateCalculator.cs
+++ src/JoinRpg.Services.Impl/Projects/ProjectStaleDateCalculator.cs
@@ -6,37 +6,65 @@ internal class ProjectStaleDateCalculator
     private const int CloseDistanceInMonths = 7;
     private const int WarnDistanceBeforCloseInMonths = 3;
 
-    private static DateOnly MoveUntilDayOfMonth(DateOnly maxDate, int day)
+    private static DateOnly MoveUntilDayOfMonth(DateOnly dateToMove, int day)
     {
-        if (maxDate.Day > day)
+        if (dateToMove.Day == day)
         {
-            maxDate = maxDate.AddMonths(1);
+            return dateToMove;
         }
-
-        return new DateOnly(maxDate.Year, maxDate.Month, day);
+        do
+        {
+            dateToMove = dateToMove.AddDays(1);
+        }
+        while (dateToMove.Day != day
+            // Перевалили за границу месяца
+            && dateToMove.Day != 1);
+        return dateToMove;
     }
 
-    internal static (ProjectStaleStatus, DateOnly closeDate) CalculateStaleStatus(DateOnly lastUpdated, DateOnly today)
+    internal static (ProjectStaleStatus status, DateOnly closeDate) CalculateStaleStatus(DateOnly lastUpdated, DateOnly today)
     {
-        var closeCandidate = MoveUntilDayOfMonth(lastUpdated.AddMonths(CloseDistanceInMonths), lastUpdated.Day);
-        if (closeCandidate < today)
+        if (today < lastUpdated)
         {
-            closeCandidate = MoveUntilDayOfMonth(today, closeCandidate.Day);
+            throw new InvalidOperationException("Дата обновления проекта в будущем");
         }
-        if (closeCandidate == today)
-        {
-            return (ProjectStaleStatus.StaleCloseToday, closeCandidate);
-        }
-        if (today.AddMonths(WarnDistanceBeforCloseInMonths) <= closeCandidate)
+        if (lastUpdated.AddMonths(WarnDistanceBeforCloseInMonths) > today)
         {
             return (ProjectStaleStatus.NotStale, DateOnly.MaxValue);
         }
-        if (closeCandidate.Day == today.Day)
+
+        var closeCandidate = GetCloseThreshold(lastUpdated);
+
+        if (closeCandidate > today)
         {
-            return (ProjectStaleStatus.StaleWarnToday, closeCandidate);
+            // Зона предупреждения
+            for (var monthDistance = WarnDistanceBeforCloseInMonths; monthDistance < CloseDistanceInMonths; monthDistance++)
+            {
+                var warnDate = lastUpdated.AddMonths(monthDistance);
+                if (warnDate == today)
+                {
+                    return (ProjectStaleStatus.StaleWarnToday, closeCandidate);
+                }
+            }
+            return (ProjectStaleStatus.StaleWarnOtherTime, closeCandidate);
         }
-        return (ProjectStaleStatus.StaleWarnOtherTime, closeCandidate);
+        else
+        {
+            // Зона закрытия
+            if (closeCandidate.Day == today.Day)
+            {
+                return (ProjectStaleStatus.StaleCloseToday, today);
+            }
+            else
+            {
+                return (ProjectStaleStatus.StaleWarnOtherTime, closeCandidate);
+            }
+
+        }
+
     }
+
+    internal static DateOnly GetCloseThreshold(DateOnly lastUpdated) => MoveUntilDayOfMonth(lastUpdated.AddMonths(CloseDistanceInMonths), lastUpdated.Day);
 }
 
 internal enum ProjectStaleStatus
