6b1b8171fdec43e2d82b37a7b335c64e886c294f
Leonid Tsarev <leo@leotsarev.ru>
2025-11-21 12:16:44 +0300

    Добавить дополнительные логи при отправке в телеграмм

--- src/JoinRpg.PrimitiveTypes/TelegramId.cs
+++ src/JoinRpg.PrimitiveTypes/TelegramId.cs
@@ -6,4 +6,6 @@ public record TelegramId(long Id, PrefferedName? UserName)
     public static TelegramId? FromOptional(long? id, PrefferedName? userName) => id is null ? null : new TelegramId(id.Value, userName);
 
     public static TelegramId? FromOptional(string? key, PrefferedName? userName) => string.IsNullOrWhiteSpace(key) ? null : new TelegramId(long.Parse(key), userName);
+
+    public override string ToString() => UserName is null ? $"Telegram({Id})" : $"Telegram({Id}, @{UserName.Value})";
 }
