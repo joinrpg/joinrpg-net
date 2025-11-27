25093616bf41c28cf146b32af16f9fb86aaa28dc
leotsarev <leo@leotsarev.ru>
2025-11-27 18:45:16 +0300

    revert unused changes

--- src/JoinRpg.WebPortal.Models/Masters/PermissionExtensions.cs
+++ src/JoinRpg.WebPortal.Models/Masters/PermissionExtensions.cs
@@ -9,43 +9,6 @@ namespace JoinRpg.Web.Models.Masters;
 public static class PermissionExtensions
 {
     [Pure]
-    public static Func<ProjectAcl, bool> GetPermssionExpression(this Permission permission)
-    {
-        return permission switch
-        {
-            Permission.None => acl => true,
-            Permission.CanChangeFields => acl => acl.CanChangeFields,
-            Permission.CanChangeProjectProperties => acl => acl.CanChangeProjectProperties,
-            Permission.CanGrantRights => acl => acl.CanGrantRights,
-            Permission.CanManageClaims => acl => acl.CanManageClaims,
-            Permission.CanEditRoles => acl => acl.CanEditRoles,
-            Permission.CanManageMoney => acl => acl.CanManageMoney,
-            Permission.CanSendMassMails => acl => acl.CanSendMassMails,
-            Permission.CanManagePlots => acl => acl.CanManagePlots,
-            Permission.CanManageAccommodation => acl => acl.CanManageAccommodation,
-            Permission.CanSetPlayersAccommodations => acl => acl.CanSetPlayersAccommodations,
-            _ => throw new ArgumentOutOfRangeException(nameof(permission)),
-        };
-    }
-
-    [Pure]
-    public static Permission[] GetPermissions(this ProjectAcl acl)
-    {
-        return Impl().ToArray();
-
-        IEnumerable<Permission> Impl()
-        {
-            foreach (var permission in Enum.GetValues<Permission>())
-            {
-                if (permission.GetPermssionExpression()(acl))
-                {
-                    yield return permission;
-                }
-            }
-        }
-    }
-
-    [Pure]
     public static PermissionBadgeViewModel[] GetPermissionViewModels(this ProjectAcl acl)
     {
         return Enum.GetValues<Permission>()
