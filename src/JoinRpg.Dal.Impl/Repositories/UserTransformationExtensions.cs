22efb394e8649dc7e6747d83adff8be4d419d010
leotsarev <leo@leotsarev.ru>
2025-11-27 18:35:19 +0300

    remove unused methods

--- src/JoinRpg.Dal.Impl/Repositories/UserTransformationExtensions.cs
+++ src/JoinRpg.Dal.Impl/Repositories/UserTransformationExtensions.cs
@@ -12,19 +12,4 @@ public static class UserTransformationExtensions
     }
 
     public static UserDisplayName ExtractDisplayName(this User user) => new UserDisplayName(user.ExtractFullName(), new Email(user.Email));
-
-    public static UserExternalLogin? TryGetExternalLoginByProviderId(this User user, string providerId)
-    {
-        return user.ExternalLogins.SingleOrDefault(l => l.Provider.Equals(providerId, StringComparison.InvariantCultureIgnoreCase));
-    }
-
-    public static TelegramId? TryGetTelegramId(this User user)
-    {
-        var elogin = user.TryGetExternalLoginByProviderId("telegram");
-        if (elogin == null)
-        {
-            return null;
-        }
-        return new TelegramId(long.Parse(elogin.Key), PrefferedName.FromOptional(user.Extra?.Telegram));
-    }
 }
