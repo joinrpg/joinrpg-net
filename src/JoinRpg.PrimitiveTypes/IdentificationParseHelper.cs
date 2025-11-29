1186237a9714c16702315743cb8cc1162767168d
Leonid Tsarev <leo@leotsarev.ru>
2025-11-29 14:58:12 +0300

    Add TelegramId parsing (#3770)

--- src/JoinRpg.PrimitiveTypes/IdentificationParseHelper.cs
+++ src/JoinRpg.PrimitiveTypes/IdentificationParseHelper.cs
@@ -4,25 +4,9 @@ internal static class IdentificationParseHelper
 {
     internal static (int i1, int i2, int i3)? TryParse3(ReadOnlySpan<char> value, IFormatProvider? provider, params ReadOnlySpan<string> prefixes)
     {
-        var val = value.Trim();
-        foreach (var prefix in prefixes)
-        {
-            if (val.StartsWith(prefix))
-            {
-                val = val[prefix.Length..];
-            }
-        }
-        if (val.StartsWith("("))
-        {
-            val = val[1..];
-        }
-        if (val.EndsWith(")"))
-        {
-            val = val[..^1];
-        }
-
+        ReadOnlySpan<char> val = RemovePrefixes(value, prefixes);
         Span<Range> ranges = stackalloc Range[3];
-        var count = val.Split(ranges, "-", StringSplitOptions.TrimEntries);
+        var count = SplitIdentifier(val, ranges);
         if (count == 3
            && ProjectIdentification.TryParse(val[ranges[0]], provider, out var i1)
            && int.TryParse(val[ranges[1]], provider, out var i2)
@@ -37,24 +21,9 @@ internal static class IdentificationParseHelper
 
     internal static (int i1, int i2)? TryParse2(ReadOnlySpan<char> value, IFormatProvider? provider, params ReadOnlySpan<string> prefixes)
     {
-        var val = value.Trim();
-        foreach (var prefix in prefixes)
-        {
-            if (val.StartsWith(prefix))
-            {
-                val = val[prefix.Length..];
-            }
-        }
-        if (val.StartsWith("("))
-        {
-            val = val[1..];
-        }
-        if (val.EndsWith(")"))
-        {
-            val = val[..^1];
-        }
+        ReadOnlySpan<char> val = RemovePrefixes(value, prefixes);
         Span<Range> ranges = stackalloc Range[2];
-        var count = val.Split(ranges, "-", StringSplitOptions.TrimEntries);
+        var count = SplitIdentifier(val, ranges);
         if (count == 2
            && ProjectIdentification.TryParse(val[ranges[0]], provider, out var i1)
            && int.TryParse(val[ranges[1]], provider, out var i2)
@@ -67,6 +36,18 @@ internal static class IdentificationParseHelper
     }
 
     internal static int? TryParse1(ReadOnlySpan<char> value, IFormatProvider? provider, params ReadOnlySpan<string> prefixes)
+    {
+        ReadOnlySpan<char> val = RemovePrefixes(value, prefixes);
+
+        if (int.TryParse(val, provider, out var id))
+        {
+            return id;
+        }
+
+        return null;
+    }
+
+    internal static ReadOnlySpan<char> RemovePrefixes(ReadOnlySpan<char> value, ReadOnlySpan<string> prefixes)
     {
         var val = value.Trim();
         foreach (var prefix in prefixes)
@@ -85,11 +66,8 @@ internal static class IdentificationParseHelper
             val = val[..^1];
         }
 
-        if (int.TryParse(val, provider, out var id))
-        {
-            return id;
-        }
-
-        return null;
+        return val;
     }
+
+    internal static int SplitIdentifier(ReadOnlySpan<char> val, Span<Range> ranges) => val.SplitAny(ranges, "-,", StringSplitOptions.TrimEntries);
 }
--- src/JoinRpg.PrimitiveTypes/TelegramId.cs
+++ src/JoinRpg.PrimitiveTypes/TelegramId.cs
@@ -1,11 +1,47 @@
 
+using System.Diagnostics.CodeAnalysis;
+
 namespace JoinRpg.PrimitiveTypes;
 
-public record TelegramId(long Id, PrefferedName? UserName)
+public record TelegramId(long Id, PrefferedName? UserName) : ISpanParsable<TelegramId>
 {
     public static TelegramId? FromOptional(long? id, PrefferedName? userName) => id is null ? null : new TelegramId(id.Value, userName);
 
     public static TelegramId? FromOptional(string? key, PrefferedName? userName) => string.IsNullOrWhiteSpace(key) ? null : new TelegramId(long.Parse(key), userName);
 
+    public static bool TryParse([NotNullWhen(true)] ReadOnlySpan<char> value, IFormatProvider? provider, [MaybeNullWhen(false)] out TelegramId result)
+    {
+        ReadOnlySpan<char> val = IdentificationParseHelper.RemovePrefixes(value, [nameof(TelegramId), "Telegram"]);
+        Span<Range> ranges = stackalloc Range[2];
+        var count = IdentificationParseHelper.SplitIdentifier(val, ranges);
+        if (count == 2
+           && int.TryParse(val[ranges[0]], provider, out var i1)
+           )
+        {
+            result = new TelegramId(i1, PrefferedName.FromOptional(val[ranges[1]].TrimStart('@').ToString()));
+            return true;
+        }
+
+        if (count == 1
+          && int.TryParse(val[ranges[0]], provider, out var i)
+          )
+        {
+            result = new TelegramId(i, null);
+            return true;
+        }
+
+        result = null!;
+        return false;
+    }
+
+    public static TelegramId Parse(string value, IFormatProvider? provider = null) => Parse(value.AsSpan(), provider);
+
+    public static bool TryParse(string? value, IFormatProvider? provider, [MaybeNullWhen(false)] out TelegramId result) => TryParse(value.AsSpan(), provider, out result);
+
+    public static TelegramId Parse(ReadOnlySpan<char> value, IFormatProvider? provider)
+        => TryParse(value, provider, out var result) ? result : throw new ArgumentException("Could not parse supplied value.", nameof(value));
+
+
     public override string ToString() => UserName is null ? $"Telegram({Id})" : $"Telegram({Id}, @{UserName.Value})";
+
 }
