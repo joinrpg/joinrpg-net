404028b3856b4b887eba7533b4a66a223a932718
Leonid Tsarev <leo@leotsarev.ru>
2025-12-03 00:22:18 +0300

    Исправить миграцию

---
+++ src/JoinRpg.Dal.Notifications/Migrations/20251202212153_TelegramEnum.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JoinRpg.Dal.Notifications.Migrations
+{
+    /// <inheritdoc />
+    public partial class TelegramEnum : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:notification_channel", "email,show_in_ui,telegram")
+                .Annotation("Npgsql:Enum:notification_message_status", "failed,queued,sending,sent")
+                .OldAnnotation("Npgsql:Enum:notification_channel", "email,show_in_ui")
+                .OldAnnotation("Npgsql:Enum:notification_message_status", "failed,queued,sending,sent");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:Enum:notification_channel", "email,show_in_ui")
+                .Annotation("Npgsql:Enum:notification_message_status", "failed,queued,sending,sent")
+                .OldAnnotation("Npgsql:Enum:notification_channel", "email,show_in_ui,telegram")
+                .OldAnnotation("Npgsql:Enum:notification_message_status", "failed,queued,sending,sent");
+        }
+    }
+}
