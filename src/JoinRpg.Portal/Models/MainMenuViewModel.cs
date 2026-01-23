068ca407e37dc40d3e16c46dc5adecea5bafe0f9
Leonid Tsarev <leo@leotsarev.ru>
2026-01-23 10:44:04 +0300

    Переместить загрузку blazor в layout (#3834)

--- src/JoinRpg.Portal/Models/MainMenuViewModel.cs
+++ src/JoinRpg.Portal/Models/MainMenuViewModel.cs
@@ -2,9 +2,4 @@ using JoinRpg.PrimitiveTypes.ProjectMetadata;
 
 namespace JoinRpg.Portal.Models;
 
-public class MainMenuViewModel
-{
-    public required ProjectShortInfo[] ProjectLinks { get; set; }
-    public required int? CurrentProjectId { get; set; }
-    public required string? CurrentProjectName { get; set; }
-}
+public record class MainMenuViewModel(ProjectShortInfo[] ProjectLinks, string? CurrentProjectName);
