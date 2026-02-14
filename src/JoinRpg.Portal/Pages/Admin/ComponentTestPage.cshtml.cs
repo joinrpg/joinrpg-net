285d6ec16ec19c7dcbb94d22ddfba65d6a9e7903
Leonid Tsarev <leo@leotsarev.ru>
2026-02-14 11:33:30 +0300

    Добавить авторизацию на уровне UI в админские страницы (#3909)

--- src/JoinRpg.Portal/Pages/Admin/ComponentTestPage.cshtml.cs
+++ src/JoinRpg.Portal/Pages/Admin/ComponentTestPage.cshtml.cs
@@ -1,7 +1,9 @@
+using JoinRpg.Portal.Infrastructure.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace JoinRpg.Portal.Pages.Admin;
 
+[AdminAuthorize]
 public class ComponentTestPage : PageModel
 {
     public void OnGet()
--- src/JoinRpg.Portal/Pages/Admin/Index.cshtml.cs
+++ src/JoinRpg.Portal/Pages/Admin/Index.cshtml.cs
@@ -1,7 +1,9 @@
+using JoinRpg.Portal.Infrastructure.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace JoinRpg.Portal.Pages.Admin;
 
+[AdminAuthorize]
 public class IndexModel : PageModel
 {
     public void OnGet()
