a973c9c07b67c09154972f66e579b15a1eaf17c4
Leonid Tsarev <leo@leotsarev.ru>
2026-07-31 18:36:42 +0300

    Update ClaimServiceImpl.cs

--- src/JoinRpg.Services.Impl/Claims/ClaimServiceImpl.cs
+++ src/JoinRpg.Services.Impl/Claims/ClaimServiceImpl.cs
@@ -757,8 +757,7 @@ internal class ClaimServiceImpl(
         _ = projectInfo.RequestMasterAccess(responsibleMasterId);
 
         var oldResponsibleMaster = new UserIdentification(claim.ResponsibleMasterUserId);
-        var oldMasterDisplayName = claim.ResponsibleMasterUser?.GetDisplayName()
-            ?? (await UserRepository.GetById(oldResponsibleMaster)).GetDisplayName();
+        var oldMasterDisplayName = claim.ResponsibleMasterUser.GetDisplayName();
 
         if (responsibleMasterId == oldResponsibleMaster)
         {
