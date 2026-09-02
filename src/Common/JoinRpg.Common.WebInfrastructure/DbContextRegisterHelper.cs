d2e03770152e88b37097b8fb74afa0b4eb5c5e00
leotsarev <leo@leotsarev.ru>
2026-09-02 18:58:56 +0300

    fix: отключить попытку GSSAPI-шифрования Npgsql к Postgres
    Npgsql 10 по умолчанию пытается согласовать GSSAPI/Kerberos-шифрование
    при каждом подключении, а krb5-библиотек в контейнере нет — из-за
    этого в логе при старте появляется безобидная, но пугающая ошибка
    "Cannot load library libgssapi_krb5.so.2". Kerberos для Postgres не
    используется, поэтому отключаем эту попытку явно в connection string.
    
    См. https://github.com/npgsql/npgsql/issues/6360 и
    https://github.com/dotnet/runtime/issues/126251 (фикс в .NET 11).

--- src/Common/JoinRpg.Common.WebInfrastructure/DbContextRegisterHelper.cs
+++ src/Common/JoinRpg.Common.WebInfrastructure/DbContextRegisterHelper.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Logging;
+using Npgsql;
 
 namespace JoinRpg.Common.WebInfrastructure;
 
@@ -22,6 +23,16 @@ public static class DbContextRegisterHelper
         {
             return false;
         }
+
+        // Мы не используем Kerberos-аутентификацию к Postgres, а krb5-библиотек в контейнере нет
+        // (см. https://github.com/npgsql/npgsql/issues/6360). Без явного отключения Npgsql 10
+        // всё равно пытается их найти при каждом подключении и пишет в лог не влияющую ни на что ошибку.
+        // В .NET 11 сам рантайм перестанет логировать эту ошибку (https://github.com/dotnet/runtime/issues/126251),
+        // но это отключение можно будет убрать не раньше перехода проекта на .NET 11.
+        connectionString = new NpgsqlConnectionStringBuilder(connectionString)
+        {
+            GssEncryptionMode = GssEncryptionMode.Disable,
+        }.ConnectionString;
 
         services.AddDbContext<TContext>(
         options =>
