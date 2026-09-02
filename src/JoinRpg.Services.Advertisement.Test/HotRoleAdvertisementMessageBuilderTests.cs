28554657794a53d7a39eab4f0250b3e6a96a3f39
leotsarev <leo@leotsarev.ru>
2026-09-02 14:26:56 +0300

    feat: добавить ссылку на ВК игры в рекламное сообщение горячей роли
    Если у карточки КогдаИгры заполнено поле VkClub, ссылка на группу ВК
    добавляется в текст объявления, отправляемого в Telegram-канал.

--- src/JoinRpg.Services.Advertisement.Test/HotRoleAdvertisementMessageBuilderTests.cs
+++ src/JoinRpg.Services.Advertisement.Test/HotRoleAdvertisementMessageBuilderTests.cs
@@ -57,4 +57,33 @@ public class HotRoleAdvertisementMessageBuilderTests
 
         return Verify(message.Contents);
     }
+
+    [Fact]
+    public Task BuildMessage_WhenVkClubIsSet_IncludesVkLink()
+    {
+        var uri = new Uri("https://joinrpg.ru/character/1/claim");
+
+        var kogdaIgraGame = new KogdaIgraGameData(
+            new KogdaIgraIdentification(1),
+            "Зимний путь 2026",
+            new DateOnly(2026, 3, 1),
+            new DateOnly(2026, 3, 5),
+            "Московская область",
+            "",
+            SiteUri: null,
+            IsActive: true,
+            VkClub: new VkId("club219947794"));
+
+        var kogdaIgraCardUri = new Uri("https://kogda-igra.ru/game/1/");
+
+        var message = TelegramSingleHotRoleSender.BuildMessage(
+            "Тёмный властелин",
+            "Зимний путь",
+            new MarkdownString("Описание персонажа"),
+            kogdaIgraGame,
+            kogdaIgraCardUri,
+            uri);
+
+        return Verify(message.Contents);
+    }
 }
